d1d77cb47f2d0e16e07dc32cc6a686f91b9563b3
David Piccolella <davepiccolella@gmail.com>
2017-02-27 10:25:32 -0500

    Starting combined interface

---
+++ src/components/WhiteboardMessager.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container" style="margin-top:20px;">
+  	<div class = "col-sm-8">
+    	<whiteboard></whiteboard>
+    </div>
+    <div class= "col-sm-4">
+    	<messager></messager>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+
+import Whiteboard from './Whiteboard';
+import Messager from './Messager';
+
+export default {
+  components: {
+    Whiteboard,
+    Messager
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -16,6 +16,7 @@ import LoginForm from './components/LoginForm'
 import Registration from './components/Registration'
 import Onboarding from './components/Onboarding'
 import Whiteboard from './components/Whiteboard'
+import WhiteboardMessager from './components/WhiteboardMessager'
 import Messager from './components/Messager'
 import Action from './components/Action'
 import Profile from './components/Profile'
@@ -32,6 +33,7 @@ const routes = [
   { path: '/signup', component: Registration },
   { path: '/whiteboard', component: Whiteboard, meta: { protected: false } },
   { path: '/messager', component: Messager, meta: {protected: false}},
+  { path: '/whiteboardmessager', component: WhiteboardMessager, meta: { protected: false } },
   { path: '/action/:action/:data?', component: Action, meta: { bypassOnboarding: true } },
   { path: '/onboarding/:step?', component: Onboarding, meta: { protected: true } },
   { path: '/profile', component: Profile, meta: { protected: true } }
