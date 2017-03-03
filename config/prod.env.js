e358932363998b00c23668a2f32075ff982c990d
Corey <corey@x64.co>
2017-03-03 00:12:14 -0800

    Fixing typo

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 module.exports = {
   NODE_ENV: '"production"',
-  SERVER_ROOT: '"http://138.197.113.45/:3000"',
-  SOCKET_ADDRESS: '"http://138.197.113.45/:3001"'
+  SERVER_ROOT: '"http://138.197.113.45:3000"',
+  SOCKET_ADDRESS: '"http://138.197.113.45:3001"'
 }
