4a0d82fdfa82dc94aa3e23fef3e57029b80e069e
alymurray <Aly@Alysas-MacBook-Air.local>
2017-03-04 21:30:56 -0500

    header

--- src/components/Resources.vue
+++ src/components/Resources.vue
@@ -1,5 +1,10 @@
-<template>
-</template>
+
+  <div class="row">
+
+  	<div class="col-sm-12" style="background-color:rgba(255,208,115,.6); text-align:left; padding-left:50px;">
+  		<h3 style="color:white"><strong>Privacy Policy</strong></h3>
+    </div>
+  </div>
 
 <script>
 export default {
