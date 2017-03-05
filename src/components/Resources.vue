6716c3de2913f7b10315852524bd5ea735badb7d
alymurray <Aly@Alysas-MacBook-Air.local>
2017-03-05 16:38:16 -0500

    adding "template" tags back into Privacy.vue
    This reverts commit 714c5627530f1940f2aff76d8f5d04c632f3dcc1, reversing
    changes made to 7ff17438b8d120da591bc57eea719f08f21f1e18.

--- src/components/Resources.vue
+++ src/components/Resources.vue
@@ -2,11 +2,11 @@
   <div class="row">
 
   	<div class="col-sm-12" style="background-color:rgba(255,208,115,.6); text-align:left; padding-left:50px;">
-  		<h3 style="color:white"><strong>Useful Resources</strong></h3>
+  		<h3 style="color:white"><strong>Resources</strong></h3>
     </div>
   </div>
 
-  
+
 </template>
 <script>
 export default {
