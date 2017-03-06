2fb1e15718ae64035488047477183ab834bea622
alysamurray <murray.alysa@gmail.com>
2017-03-06 07:59:14 -0500

    fixing feedback link

--- src/components/Feedback.vue
+++ src/components/Feedback.vue
@@ -2,7 +2,7 @@
 	<div class="row">
 		<div class="col-sm-12">
 		<template v-if="!user.isVolunteer">
-			<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfpLjDDdjw9eE9GAg9r-AMotDo-SRlM1eO9h6k9Mw5ZSp0J_Q/viewform?embedded=true" width="1000" height="750" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+			<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSf3k-NkzCSvCfy-ywt1ayu2rHYGOIfts8gcaJPINROoywR6rg/viewform?embedded=true" width="1000" height="750" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
 		</template>
 		<template v-else>
 			<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSc9doxOTqCcf5286LWPl3SYuJalBw3MurgkRJvrsod8ZId0tw/viewform?embedded=true" width="1000" height="750" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
