93e7a291b9bf7c2cfc8af18814a850ccca61200e
Corey <corey@x64.co>
2017-03-09 14:59:08 -0800

    HTTPS for prod deployment

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 module.exports = {
   NODE_ENV: '"production"',
-  SERVER_ROOT: '"http://138.197.113.45:3000"',
-  SOCKET_ADDRESS: '"http://138.197.113.45:3001"',
+  SERVER_ROOT: '"https://138.197.113.45:3000"',
+  SOCKET_ADDRESS: '"https://138.197.113.45:3001"',
   MAIN_WEBSITE_URL: '"http://www.upchieve.org"'
 }
