86a93862f28396e379c488b7a8a1f59b12baebf4
alysamurray <aly@Alys-MacBook-Pro.local>
2017-03-11 20:26:20 -0500

    Google form width change

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -3,7 +3,7 @@
 	<div class="row">
 
   	<div class="col-sm-12">
-  		<iframe src="https://docs.google.com/forms/d/e/1FAIpQLScP9FUKeeH869Z1x4vk4JxyYFiPnDHwVKZAMYGa5eErOGhnFw/viewform?embedded=true" width="1000" height="750" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+  		<iframe src="https://docs.google.com/forms/d/e/1FAIpQLScP9FUKeeH869Z1x4vk4JxyYFiPnDHwVKZAMYGa5eErOGhnFw/viewform?embedded=true" width="100%" height="1000" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
   	</div>
   </div>
 </template>
