67500b115b07e8036e7465f9246a450f48df93df
ilakumar <ilakkumar@gmail.com>
2017-07-15 17:45:04 -0400

    display first question and options

--- src/components/Training/Quiz.vue
+++ src/components/Training/Quiz.vue
@@ -1,15 +1,17 @@
 <template>
   <div v-if="user.isVolunteer" class="training-quiz">
-    <div class="header" id="quiz-name"></div>
-    <div class="question">{{ questionText }}</div>
+    <h1 class="header" id="quiz-name">{{ quizName }}</h1>
+    <div class="questionText">{{ questionText }}</div>
+    <form class="possibleAnswers"">
+      <input class="option-0" type="radio">{{ option0 }}
+      <input class="option-1" type="radio">{{ option1 }}
+      <input class="option-2" type="radio">{{ option2 }}
+      <input class="option-3" type="radio">{{ option3 }}
+    </form>
     <button id="start-question-button" type="start" @click.prevent="start()">Start Quiz</button>
     <button id="prev-question-button">Previous</button>
     <button id="next-question-button" type="next" @click.prevent="next()">Next Question</button>
     <button id="submit-button">Submit Answers</button>
-    <div id="quiz-results">
-      <p id="quiz-results-message"></p>
-      <p id="quiz-results-score"></p>
-    </div>
   </div>
 </template>
 
@@ -23,17 +25,29 @@ export default {
     let user = UserService.getUser();
     return {
       user: user,
-      questionText: 'Click the Start button to begin the quiz!'
+      questionText: 'Click the Start button to begin the quiz!',
+      quizName: 'Quiz',
+      option0: '',
+      option1: '',
+      option2: '',
+      option3: ''
     }
   },
   methods: {
     start(){
-      TrainingService.displayQuestion(this).then((questionText) => {
-        this.questionText = questionText;
+      TrainingService.displayQuestion(this).then((question) => {
+        this.questionText = question.questionText;
+        this.option0 = question.possibleAnswers[0];
+        this.option1 = question.possibleAnswers[1];
+        this.option2 = question.possibleAnswers[2];
+        this.option3 = question.possibleAnswers[3];
       });
     },
     next(){
 
+    },
+    submit(){
+    
     }
   }
 }
