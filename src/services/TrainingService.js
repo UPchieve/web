966a19f9a29078dd02ce1ef717df0c4b02bc4e1e
ilakumar <ilakkumar@gmail.com>
2017-07-15 09:53:56 -0400

    display first question of quiz

--- src/services/TrainingService.js
+++ src/services/TrainingService.js
@@ -3,8 +3,8 @@ import NetworkService from './NetworkService'
 export default {
   displayQuestion(context){
     return NetworkService.getQuestion(context).then((res) => {
-      let question = res.data.question;
-      return question;
+      let questionText = res.data.question.questionText;
+      return questionText;
     })
   },
 }
