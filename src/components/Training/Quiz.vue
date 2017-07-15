16212d8509590656d0a2675eacf6703133f4e969
ilakumar <ilakkumar@gmail.com>
2017-07-15 09:53:56 -0400

    display first question of quiz

--- src/components/Training/Quiz.vue
+++ src/components/Training/Quiz.vue
@@ -1,7 +1,8 @@
 <template>
   <div v-if="user.isVolunteer" class="training-quiz">
     <div class="header" id="quiz-name"></div>
-    <button id="start-question-button" type="start" @click.prevent="start()">Start</button>
+    <div class="question">{{ questionText }}</div>
+    <button id="start-question-button" type="start" @click.prevent="start()">Start Quiz</button>
     <button id="prev-question-button">Previous</button>
     <button id="next-question-button" type="next" @click.prevent="next()">Next Question</button>
     <button id="submit-button">Submit Answers</button>
@@ -19,17 +20,20 @@ import TrainingService from 'src/services/TrainingService';
 
 export default {
   data() {
-    var user = UserService.getUser();
+    let user = UserService.getUser();
     return {
       user: user,
+      questionText: 'Click the Start button to begin the quiz!'
     }
   },
   methods: {
     start(){
-      var question = TrainingService.displayQuestion(this);
-      
+      TrainingService.displayQuestion(this).then((questionText) => {
+        this.questionText = questionText;
+      });
     },
     next(){
+
     }
   }
 }
