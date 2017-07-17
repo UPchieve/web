f0b2780f621ceda6d0a9453ded72646657a24ac8
ilakumar <ilakkumar@gmail.com>
2017-07-17 00:14:34 -0400

    disable and enable buttons

--- src/services/TrainingService.js
+++ src/services/TrainingService.js
@@ -4,15 +4,25 @@ export default {
   questions: [],
   index: 0,
   startQuiz(context){
+    this.index = 0;
     return NetworkService.getQuestions(context).then((res) => {
       this.questions = res.data.questions;
       var question = this.questions[this.index];
       console.log(this.questions);
       console.log('first question: ', question);
+      console.log(this.questions);
       return question;
-    })
+    });
   },
-  getNextQuestion(context){
+  hasNext(context){
+    console.log('has next');
+    return ((this.index + 1) < this.questions.length);
+  },
+  hasPrevious(context){
+    console.log('has previous');
+    return ((this.index) > 0);
+  },
+  getNextQuestion(context, picked){
     if (this.index < this.questions.length) {
       this.index = this.index + 1;
       var question = this.questions[this.index];
@@ -20,5 +30,28 @@ export default {
       return question;
     }
     return null;
+  },
+  getPreviousQuestion(context){
+    if (this.index > 0) {
+      this.index = this.index - 1;
+      var question = this.questions[this.index];
+      console.log(question);
+      return question;
+    }
+    return null;
+  },
+  saveAnswer(context, picked){
+    var question = this.questions[this.index];
+    question.picked = picked;
+    return;
+  },
+  submitQuiz(context){
+    var correctAnswers = 0;
+    this.questions.forEach(function(item, index){
+      if (item.picked == item.correctAnswer) {
+        correctAnswers = correctAnswers + 1;
+      }
+    });
+    return correctAnswers;
   }
 }
