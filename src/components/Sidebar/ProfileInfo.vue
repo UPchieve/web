37310d0120e2e2bdebd784c5ffa67bc22adbdc65
Will <willaftring@gmail.com>
2018-02-02 11:31:32 -0500

    Revert changes

--- src/components/Sidebar/ProfileInfo.vue
+++ src/components/Sidebar/ProfileInfo.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="profile-info">
     <div class="avatar" v-bind:style="avatarStyle"></div>
-    <p class="greeting">{{name}}</p>
+    <p class="greeting" v-else-if="auth.authenticated">
+      {{name}}
+    </p>
   </div>
 </template>
 
