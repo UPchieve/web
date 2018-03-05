159a94b54697a0b57bc51489a08b303476b06b91
Will Aftring <willaftring@gmail.com>
2018-03-04 21:13:52 -0500

    Sidebar fix

---
+++ config/prod.env copy.js
@@ -0,0 +1,6 @@
+module.exports = {
+  NODE_ENV: '"production"',
+  SERVER_ROOT: '"https://app.upchieve.org"',
+  SOCKET_ADDRESS: '"https://app.upchieve.org:3001"',
+  MAIN_WEBSITE_URL: '"http://www.upchieve.org"'
+}
