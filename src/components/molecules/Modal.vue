db92d2c449cfb9c0851faf946ad61f3c4896afff
Lual <lualps94@gmail.com>
2018-03-29 17:08:50 -0400

    Initial styles for BasicTemplate component done

--- src/components/molecules/Modal.vue
+++ src/components/molecules/Modal.vue
@@ -1,78 +1,78 @@
 <template>
-  <div :class="classes">
-    <div :class="dialogClasses">
-      <div class="v-modal-dialog__content">
-        {{ message }}
-      </div>
-      <div class="v-modal-dialog__options">
-        <btn rowMember :label="secondBtnLabel" :clickHandler="clickHandlers.second"></btn>
-        <btn :label="mainBtnLabel" :clickHandler="clickHandlers.main"></btn>
-      </div>
-    </div> 
+<div :class="classes">
+  <div :class="dialogClasses">
+    <div class="v-modal-dialog__content">
+      {{ message }}
+    </div>
+    <div class="v-modal-dialog__options">
+      <btn rowMember :label="secondBtnLabel" :clickHandler="clickHandlers.second"></btn>
+      <btn :label="mainBtnLabel" :clickHandler="clickHandlers.main"></btn>
+    </div>
   </div> 
+</div> 
 </template>
 
 
 <script>
-  import Btn from '../atoms/Btn';
+import Btn from '../atoms/Btn';
 
-  export default {
-    components: {
-      Btn
-    },
-    props: {
-      message: String,
-      warn: Boolean,
-      labels: Array[String],
-      clickHandlers: Object
-    },
-    data() {
-      return {
-        mainBtnLabel: this.labels[1],
-        secondBtnLabel: this.labels[0],
-        classes: {
-          'v-modal': true,
-          'v-modal--warn': this.warn
-        },
-        dialogClasses: {
-          'v-modal-dialog': true,
-          'v-modal-dialog--warn': this.warn
-        }
+export default {
+  components: {
+    Btn
+  },
+  props: {
+    message: String,
+    warn: Boolean,
+    labels: Array[String],
+    clickHandlers: Object
+  },
+  data() {
+    return {
+      mainBtnLabel: this.labels[1],
+      secondBtnLabel: this.labels[0],
+      classes: {
+        'v-modal': true,
+        'v-modal--warn': this.warn
+      },
+      dialogClasses: {
+        'v-modal-dialog': true,
+        'v-modal-dialog--warn': this.warn
       }
     }
   }
+}
 </script>
 
 
 <style>
-  .v-modal {
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    z-index: 1000;
-  }
-  .v-modal--warn {
-    background: var(--c-backdrop-warn);
-  }
+.v-modal {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1000;
+}
+.v-modal--warn {
+  background: var(--c-backdrop-warn);
+}
 
-  .v-modal-dialog {
-    background: #fff;
-    width: 80%;
-    max-width: 480px;
-    padding: 40px;
-  }
-  .v-modal-dialog__content {
-    margin-bottom: 40px;
-  }
-  .v-modal-dialog__options {
-  
-  }
-  .v-modal-dialog--warn {
-    box-shadow: -4px 4px var(--c-shadow-warn);
-  }
+.v-modal-dialog {
+  background: #fff;
+  width: 80%;
+  max-width: 480px;
+  padding: 40px;
+}
+.v-modal-dialog__content {
+  margin-bottom: 40px;
+}
+.v-modal-dialog__options {
+
+}
+.v-modal-dialog--warn {
+  box-shadow: -4px 4px var(--c-shadow-warn);
+}
 </style>
--- src/components/views/SubmitQuestion.vue
+++ src/components/views/SubmitQuestion.vue
@@ -1,38 +1,94 @@
 <template>
-  <div class="SubmitQuestion">
-    
+<div class="submit-question">
+  <div class="basic-template">
+    <div class="basic-template__header">
+      <h1>{{ title }}</h1>
+    </div>
+    <div class="basic-template__content">
+      <div class="content-card">
+        <div class="content-card__card">
+          This is the content
+          Lorem ipsum dolor 
+          lipsum150
+        </div>
+      </div>
+    </div>
   </div>
+</div>
 </template>
 
 
 <script>
-  import Modal from '../molecules/Modal';
-  
-  export default {
-    components: {
-      Modal
-    },
-    data() {
-      return {
-        
-      }
-    },
-    methods: {
-      goHome() {
-        this.$router.push('/');
-      }
+import Modal from '../molecules/Modal';
+
+export default {
+  components: {
+    Modal
+  },
+  data() {
+    return {
+      title: 'Submit a question' 
+    }
+  },
+  methods: {
+    goHome() {
+      this.$router.push('/');
     }
   }
+}
 </script>
 
 
 <style>
-  /*
-  * @notes
-  * [1] Refactoring candidate: these styles should be placed in the container
-  *     (we need to rethink the containing model in order to do so)
-  */
-  .SubmitQuestion {
-    
+.submit-question {
+  height: 100vh;
+}
+
+/*
+* @notes
+* [1] This shoudln't be done, but I had to do it since Bootstap interferes with
+*     this :/
+*/
+.basic-template {
+  min-height: 100vh;
+  background: var(--c-backdrop);
+}
+.basic-template__header {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #fff;
+  padding: 20px;
+}
+.basic-template__header h1 { /* [1] */
+  font-size: 20px;
+  margin: 0;
+  font-weight: bold;
+}
+.basic-template__content {
+
+}
+
+.content-card {
+  padding: 80px 40px;
+  width: 100%;
+}
+.content-card__card {
+  background: #fff;
+  width: 80%;
+  max-width: 700px;
+  padding: 40px;
+  box-shadow: -4px 4px var(--c-shadow);
+  margin: auto;
+}
+
+
+@media screen and (min-width: 700px) {
+  .basic-template__header {
+    justify-content: flex-start;
   }
+  .basic-template__header h1 {
+    font-size: 28px;
+  }
+}
 </style>
