9891ee700a4d6a8c53d4e6673f102fd171e9d71c
Lual <lualps94@gmail.com>
2018-08-28 00:30:22 -0400

    Updated ESLint dependencies and configured it to use Airbnb rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,22 @@
+/**
+ * @note {0} Why this config: https://bit.ly/2oiXnf7
+ * @note {1} To allow debugger during development
+ */
 module.exports = {
-  root: true,
-  parser: 'babel-eslint',
-  parserOptions: {
-    sourceType: 'module'
+  "root": true,
+  "parserOptions": {
+    "sourceType": "module",
+    "parser": "babel-eslint",
   },
-  // required to lint *.vue files
-  plugins: [
-    'html'
+  "extends": [
+    "airbnb-base",
+    "plugin:vue/recommended"
   ],
-  // add your custom rules here
-  'rules': {
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-    "eol-last": 1,
+  "rules": {
+    "no-debugger": process.env.NODE_ENV === "production" ? 2 : 0, // {1}
     "no-console": 0,
     "no-underscore-dangle": 0,
     "no-shadow": 0,
     "strict": 2,
-    "quotes": [1, "single"]
   }
 }
