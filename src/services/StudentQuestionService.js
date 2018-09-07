c7779f7b10d25f387a286fe49aa1c25664e65d1a
Lual <lualps94@gmail.com>
2018-09-06 22:55:37 -0400

    Linted StudentQuestionService

--- src/services/StudentQuestionService.js
+++ src/services/StudentQuestionService.js
@@ -3,7 +3,7 @@ import NetworkService from './NetworkService';
 export default {
   createStudentQuestion(context, data) {
     return NetworkService.createStudentQuestion(context, data).then(
-      res => 'sentOK',
+      () => 'sentOK',
       (err) => {
         console.error(new Error('Unable to create a new StudentQuestion'));
         console.log(err);
@@ -24,7 +24,7 @@ export default {
 
   answerStudentQuestion(context, data) {
     return NetworkService.answerStudentQuestion(context, data).then(
-      res => 'sentOK',
+      () => 'sentOK',
       (err) => {
         console.error(new Error('Unable to send answer'));
         console.log(err);
