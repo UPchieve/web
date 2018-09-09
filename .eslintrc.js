e7bba3dbca7d9e37990f4fc18e411a2c6ffd3b5c
Lual <lualps94@gmail.com>
2018-09-08 22:19:41 -0400

    Updated .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,26 @@
+/** Regular expressions for exceptions to the max-len rule
+ *
+ * @note {1} Ignore HTML elements. Keep in mind that the rule doesn't support
+ *           mutiline regex, thus you have to make sure to add the closing tag
+ *           at the end of your line to prevent the error.
+ * @note {2} Ignore the single line methods in NetworkService. This regex could
+ *           theoretically be more concise, but for some reason ESLint refused
+ *           to recognize some quantifiers and literals here.
+ * @note {3} Ignore opening HTML tags that only contain a single attribute with
+ *           a long value.
+ */
+const listOfPatterns = [
+  '((<.*>)*(\n?\s*)(.*)(\n?\s*)<\/.*>)',  // {1}
+  '(\ \ \ \ return\ context\..*http.*;)', // {2} 
+  '(<[a-z]*-?\ ?.*>)',                    // {3}
+];
+const theOnePatternToRuleThemAll = listOfPatterns.join('|');
+
 /**
  * @note {0} Why this config: https://bit.ly/2oiXnf7.
  * @note {1} To allow debugger during development.
  * @note {2} To allow modifying properties of components passed as arguments.
- * @note {3} First group: ignore HTML elements. Keep in mind that the rule 
- *           doesn't support mutiline regex, thus you have to make sure to add 
- *           the closing tag at the end of your line to prevent the error.
- *           Second group: ignore the single line methods in NetworkService. 
- *           This regex could theoretically be more concise, but for some reason
- *           ESLint refused to recognize some quantifiers and literals here.
+ * @note {3} Specific patterns are documented above. 
  * @note {4} The actual max length is 80 columns. The extra padding is meant to
  *           allow you to finish an expression in the same line if it only goes
  *           a few characters beyond the 80th column AND if breaking the 
@@ -41,7 +54,7 @@ module.exports = {
     "max-len": [ 
       "error", 
       {
-        "ignorePattern": "((<.*>)*(\n?\s*)(.*)(\n?\s*)<\/.*>)|(\ \ \ \ return\ context\..*http.*;)", // {3}
+        "ignorePattern": theOnePatternToRuleThemAll, // {3}
         "code": 85, // {4}    
         "ignoreStrings": true,
       } 
