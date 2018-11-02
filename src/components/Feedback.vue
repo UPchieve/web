2d7fa81d9771b4af85b0cbd931f934c0a85d0460
zuoyuanh <david@collegemate.me>
2018-11-02 16:14:59 -0700

    bug fix for student/volunteer id

--- src/components/Feedback.vue
+++ src/components/Feedback.vue
@@ -164,8 +164,6 @@ export default {
 		this.userType = this.$route.params.userType;
 		this.studentId = this.$route.params.studentId;
 		this.volunteerId = this.$route.params.volunteerId;
-		console.log('student ' + this.studentId);
-		console.log('volunteer ' + this.volunteerId);
 		if (this.userType === 'student') {
 			this.questions = this.student_questions;
 	  } else {
--- src/components/Session/Header.vue
+++ src/components/Session/Header.vue
@@ -41,6 +41,8 @@
 import UserService from 'src/services/UserService';
 import SessionService from 'src/services/SessionService';
 
+import router from '../../router';
+
 const DEFAULT_AVATAR_URL = 'static/defaultAvatar@2x.png';
 
 /**
@@ -74,10 +76,25 @@ export default {
   },
   methods: {
     end() {
-      const result = window.confirm('Do you really want to end the session?'); // {1}
+      let studentId = '';
+      let volunteerId = null;
+      let sessionId = SessionService.currentSession.sessionId;
+      if (SessionService.currentSession && SessionService.currentSession.data.student) {
+        studentId = SessionService.currentSession.data.student._id;
+      }
+      if (SessionService.currentSession && SessionService.currentSession.data.volunteer) {
+        volunteerId = SessionService.currentSession.data.volunteer._id;
+      }
+      const result = window.confirm('Do you really want to end the session?');
       if (result) {
-        this.$socket.disconnect();
-        SessionService.endSession();
+        if (volunteerId) {
+          this.$socket.disconnect();
+          SessionService.endSession({ skipRoute: true });
+          const url = '/feedback/' + sessionId + '/' + (UserService.getUser().isVolunteer ? 'volunteer' : 'student') + '/' + studentId + '/' + volunteerId;
+          router.replace(url);
+        } else {
+          router.replace('/');
+        }
       }
     },
   },
