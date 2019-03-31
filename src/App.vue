2144a0156ec47831b168ef63b43ca8b391bff7ed
CodingMissDaisy <codingmissdaisy@anniecannons.com>
2019-03-31 19:34:35 -0400

    3/31: Adding hamburger menu/functionality for mobile view

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,28 @@
 <template>
   <div id="app">
-    <div class="nav-container">
-      <sidebar />
+
+    <div v-if="['LoginForm', 'Logout', 'Registration', 'ResetPasswordForm', 'SetPasswordForm', 'Feedback', 'Session-math', 'Session-college'].indexOf($route.name) > -1">
     </div>
+
+    <div v-else class="toggleMenu forMobileView">
+      <button 
+        v-if="!isActive" 
+        @click="toggleMenu()" 
+        class="sidebar-hamburger" 
+        v-bind:class="{white: ['Dashboard'].indexOf($route.name) > -1}"
+      />
+  
+      <button v-else  @click="toggleMenu()" class="sidebar-exit" />
+    </div>
+    
+      <div v-if="!mobileMode"  class="nav-container">
+        <sidebar v-on:closeMenu="doNothing()" />
+      </div>
+
+      <div v-else class="nav-container forMobileMode" v-bind:class="{active: isActive}">
+        <sidebar v-on:closeMenu="closeMenu()" />
+      </div>
+
     <div class="col-xs-12 view-container">
       <router-view />
     </div>
@@ -29,6 +49,29 @@ export default {
   },
   created () {
     AuthService.checkAuth(this) // {1}
+
+    if (window.innerWidth <= 488) {
+      this.mobileMode = true   
+    } else {
+      this.mobileMode = false
+    }
+  },
+  data () {
+    return {
+      mobileMode: false,
+      isActive: false
+    }
+  },
+  methods: {
+    toggleMenu() {
+        this.isActive = !this.isActive
+    },
+    closeMenu() {
+        this.isActive = false
+    },
+    doNothing() {
+      return
+    }
   }
 }
 </script>
@@ -37,6 +80,12 @@ export default {
 /*
  * @todo {1} Make this truly responsive
  */
+
+
+.toggleMenu.forMobileView {
+  display: none;
+}
+
 html,
 body,
 #app {
@@ -55,6 +104,10 @@ body,
 .nav-container,
 .view-container {
   height: 100%;
+}
+
+.toggleMenu {
+  display: none;
 }
 
 .nav-container {
@@ -80,5 +133,69 @@ body,
     padding-left: 40px;
   }
   /*------------------------------------------------------------------quickfix*/
+}
+
+
+
+@media screen and (max-width: 488px) {
+  .toggleMenu.forMobileView {
+    display: block !important;
+    position: absolute !important;
+    top: 2em !important;
+    left: 1em !important;
+    z-index: 500 !important;
+  }
+
+  .sidebar-hamburger,
+  .sidebar-exit {
+    outline: none !important;
+  }
+
+  .sidebar-hamburger {
+    width: 2em !important;
+    height: 1.5em !important;
+    margin: 0em 0em 0em 0.5em !important;
+    background: url('./assets/tealHamburgerMenu.png') !important;
+    background-size: auto 100% !important;
+    background-position: top left !important;
+    background-repeat: no-repeat !important;
+    border: none !important;
+  }
+  .sidebar-hamburger.white {
+    background: url('./assets/whiteHamburgerMenu.png') !important;
+    background-size: auto 100% !important;
+    background-position: top left !important;
+    background-repeat: no-repeat !important;
+    border: none !important;
+  }
+
+  .sidebar-exit {
+    width: 1.5em !important;
+    height: 1.5em !important;
+    position: fixed !important;
+    /* margin-top: 1em !important; */
+    background: url('./assets/exitMenuIcon.png') !important;
+    background-size: auto 100% !important;
+    background-position: top left !important;
+    background-repeat: no-repeat !important;
+    border: none !important;
+  }
+
+  .nav-container.forMobileMode {
+    margin-left: -100% !important;
+    width: 80vw !important;
+    z-index: 20 !important;
+    transition: all 0.3s linear;
+    /* display: none !important; */
+  }
+
+  .nav-container.forMobileMode.active {
+     margin-left: 0% !important;
+     transition: all 0.3s linear;
+  }
+
+  .col-xs-12.view-container {
+    padding-left: 0px !important;
+  }
 }
 </style>
