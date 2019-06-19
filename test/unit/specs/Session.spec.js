be28be4c3c6019b16ee1706d161a8217b9fb53e8
stewartwallace1115 <email?: ... name?: ... branch?: ros_master_x86swallace3644@g.fmarion.edu>
2019-06-18 22:48:47 -0400

    test unit testing

--- test/unit/specs/Session.spec.js
+++ test/unit/specs/Session.spec.js
@@ -1,14 +1,12 @@
+var expect = require('chai').expect;
 import Vue from 'vue'
-import Session from 'src/components/Session'
+import Chat from 'src/components/Session/Chat.vue'
 
-describe('Hello.vue', () => {
-  it('should render correct contents', () => {
-    const vm = new Vue({
-      el: document.createElement('div'),
-      render: h => h(Session)
-    })
-    expect(vm.$el.querySelector('.hello h1').textContent).to.equal(
-      'Welcome to Your Vue.js App'
-    )
+describe('Chat.vue', () => {
+  it('chat warning set to true', () => {
+     const vm = new Vue(Chat).$mount()
+		vm.chatWarningIsShown = false; // Reset property
+		vm.showModerationWarning();
+    expect(vm.chatWarningIsShown).to.be.true;
   })
 })
