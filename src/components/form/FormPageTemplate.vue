83dfb4c8a7b6360579c6751c422bd349e9a6ed4a
moody <dethanyel@gmail.com>
2019-06-19 09:42:15 -0400

    add FormPageTemplate.vue

---
+++ src/components/form/FormPageTemplate.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="FormPageTemplate">
+    <img class="logo" src="~src/assets/p_logo_white.png" />
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.FormPageTemplate {
+  @include flex-container(column, center, center);
+
+  background: url('~src/assets/onboarding_background.png') no-repeat center fixed;
+  background-size: cover;
+
+  min-width: 300px;
+  min-height: 100vh;
+
+  padding: 20px;
+}
+
+.logo {
+  width: 40%;
+  max-width: 155px;
+  margin-bottom: 20px;
+}
+</style>
