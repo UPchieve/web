a9aff814a8960cdb8b54a849b96e9a018eb425f2
Justin Moody <25038653+moody@users.noreply.github.com>
2019-07-11 20:56:24 -0400

    Reorganize src folder structure (#191)
    * move routed components to views folder
    
    * rename views
    
    * remove unused component `SignupForm.vue`
    
    * rename Registration folder to Signup
    
    * organize Onboarding
    
    * add name option to OnboardingView
    
    * group ListSessions with DashboardView
    
    * organize IndexView
    
    * organize reusable components used by LegalView
    
    * rename MessageForm + organize some components
    
    * organize SessionView
    
    * add name option to SessionView
    
    * organize SignupView
    
    * move Sidebar.vue to Sidebar/index.vue
    
    * remove unused component Attachment.vue
    
    * move ContentHeader.vue to layout folder
    
    * move Btn.vue to ui folder
    
    * move BtnOptions to ui folder
    
    * group College.vue with OnboardingView
    
    * move Sidebar to layout folder
    
    * move QuestionAnswerForm to form folder
    
    * remove unused component `College.vue`
    
    * simplify imports
    
    * flatten components folder
    
    * change `src` alias to `@`, replace relative paths
    
    * fix avatar urls

---
+++ src/components/Modal.vue
@@ -0,0 +1,100 @@
+<template>
+  <div :class="classes">
+    <div :class="dialogClasses">
+      <div class="v-modal-dialog__content">
+        {{ message }}
+      </div>
+      <btn
+        v-if="singleBtn"
+        :label="mainBtnLabel"
+        :click-handler="clickHandlers.main"
+      />
+      <btn-options
+        v-else
+        :main-btn-label="mainBtnLabel"
+        :second-btn-label="secondBtnLabel"
+        :click-handlers="clickHandlers"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import BtnOptions from './BtnOptions'
+import Btn from './Btn'
+
+export default {
+  components: {
+    BtnOptions,
+    Btn
+  },
+  props: {
+    singleBtn: {
+      type: Boolean,
+      default: false
+    },
+    message: {
+      type: String,
+      default: ''
+    },
+    warn: {
+      type: Boolean,
+      default: false
+    },
+    labels: {
+      type: Array[String],
+      default: ['']
+    },
+    clickHandlers: {
+      type: Object,
+      required: true
+    }
+  },
+  data () {
+    return {
+      mainBtnLabel: this.labels[0],
+      secondBtnLabel: this.labels[1] ? this.labels[1] : null,
+      classes: {
+        'v-modal': true,
+        'v-modal--warn': this.warn
+      },
+      dialogClasses: {
+        'v-modal-dialog': true,
+        'v-modal-dialog--warn': this.warn
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.v-modal {
+  position: fixed;
+  top: 0;
+  left: 300px;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1000;
+  background: $c-backdrop;
+}
+.v-modal--warn {
+  background: $c-backdrop-warn;
+}
+
+.v-modal-dialog {
+  background: #fff;
+  width: 80%;
+  max-width: 480px;
+  padding: 40px;
+  box-shadow: -4px 4px $c-shadow;
+}
+.v-modal-dialog__content {
+  margin-bottom: 40px;
+}
+.v-modal-dialog--warn {
+  box-shadow: -4px 4px $c-shadow-warn;
+}
+</style>
