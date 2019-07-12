a78224ac60795df9a9980730c945ed7089a91ecd
Georgia Gallant <georgia_gallant@brown.edu>
2019-07-12 16:39:47 -0400

    Merging with new css format

--- src/views/ResetPasswordView.vue
+++ src/views/ResetPasswordView.vue
@@ -9,36 +9,39 @@
           <router-link to="signup" class="uc-form-header-link">Sign Up</router-link>
         </div>
       </div>
-
-      <div class="uc-form-body">
-        <div class="uc-column">
-          <label for="inputEmail" class="uc-form-label">Please enter your email address</label>
-          <input
-            id="inputEmail"
-            v-model="email"
-            type="email"
-            class="uc-form-input"
-            required
-            autofocus
-          />
+      <div v-if="this.firstTime === true">
+        <div class="uc-form-body">
+          <div class="uc-column">
+            <label for="inputEmail" class="uc-form-label">Please enter your email address</label>
+            <input
+              id="inputEmail"
+              v-model="email"
+              type="email"
+              class="uc-form-input"
+              required
+              autofocus
+            />
+          </div>
         </div>
-
+        </div>
+        <div v-else>
+          Send password reset email to {{this.email}}?
+        </div>
         <button
           class="uc-form-button"
           type="submit"
           @click.prevent="submit()"
         >
-          Enter
+          SEND
         </button>
-
         <div v-if="msg !== ''">{{ msg }}</div>
-      </div>
     </form>
   </form-page-template>
 </template>
 
 <script>
 import AuthService from '@/services/AuthService'
+import UserService from '@/services/UserService'
 import FormPageTemplate from '@/components/FormPageTemplate'
 
 export default {
@@ -47,8 +50,20 @@ export default {
   },
   data () {
     return {
+      firstTime: null,
       email: '',
       msg: ''
+    }
+  },
+  mounted () {
+    let auth = UserService.getAuth()
+    if (auth.authenticated) {
+      let user = UserService.getUser()
+      this.email = user.email
+      this.firstTime = false
+    }
+    else{
+      this.firstTime = true
     }
   },
   methods: {
