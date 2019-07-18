02fd43dc028e94b2a99254b1bd88a527f6fb5135
moody <dethanyel@gmail.com>
2019-07-18 17:27:19 -0400

    import router in App test

--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -1,10 +1,10 @@
 import { shallowMount, createLocalVue } from "@vue/test-utils";
 import VueRouter from "vue-router";
 import App from "@/App.vue";
+import router from "@/router";
 
 const localVue = createLocalVue();
 localVue.use(VueRouter);
-const router = new VueRouter();
 
 describe("App.vue", () => {
   it("renders without crashing", () => {
