88e01548315134f5a9b3ffc8bfb16483491203a8
moody <dethanyel@gmail.com>
2019-07-19 02:07:02 -0400

    simplify dev build output

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,29 @@
 const path = require("path");
+const SimpleProgressWebpackPlugin = require("simple-progress-webpack-plugin");
 
 module.exports = {
   chainWebpack: config => {
+    // Import `global.scss` into all processed scss
     const types = ["vue-modules", "vue", "normal-modules", "normal"];
     types.forEach(type =>
-      addStyleResource(config.module.rule("scss").oneOf(type))
+      config.module
+        .rule("scss")
+        .oneOf(type)
+        .use("style-resource")
+        .loader("style-resources-loader")
+        .options({
+          patterns: [path.resolve(__dirname, "./src/scss/global.scss")]
+        })
     );
+
+    // Simplify dev build output
+    if (process.env.NODE_ENV === "development") {
+      // Remove vue-cli-service's progress output
+      config.plugins.delete("progress");
+      // Add simple-progress-webpack-plugin
+      config
+        .plugin("simple-progress-webpack-plugin")
+        .use(SimpleProgressWebpackPlugin, [{ format: "compact" }]);
+    }
   }
 };
-
-function addStyleResource(rule) {
-  rule
-    .use("style-resource")
-    .loader("style-resources-loader")
-    .options({
-      patterns: [path.resolve(__dirname, "./src/scss/global.scss")]
-    });
-}
