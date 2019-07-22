e5ce242a7a109aa59a75e8ecbe0450818c7cf5b4
Georgia Gallant <georgia_gallant@brown.edu>
2019-07-22 18:15:29 -0400

    Added formatting, some metric boxes, and highlighting of cells

--- src/views/Admin/VolunteerCoverage.vue
+++ src/views/Admin/VolunteerCoverage.vue
@@ -1,29 +1,48 @@
 <template>
-<div>
-   <v-select v-model= "selected" :options="topics" @input ="calendarToArray"> </v-select> 
-   
-<div class = "calendar">
-    <div v-for="(day, index2) in availabilityTable"
-       :key = "`${index2}`">
-        <div v-for="(hour, index3) in day"
-            :key = "`${index3}`"> 
-                <div v-if="(index2 = 0)|| (index3 = 0)">
-                        {{hour}}
+<div class="volunteer-coverage">
+    <div class="header">Volunteer Coverage </div>
+        <div class="wrap-container">
+
+            <div class = "container--metric"> 
+                <div class="subheader--metric"># hours w/ less than 
+                    <input v-model = "lessThan" placeholder="number" class = "input">
+                    volunteers
+                </div> 
+                <div class = "container-content"> 
+                    <div class= "metrics-info" >{{getNumHours(true)}} </div>
                 </div>
-                <div v-else>
-                    <div class = 'hour' :style = "{'--hour': getGradient(hour)}">
-                        {{hour}}
+            </div>
+
+            <div class = "container--metric"> 
+                <div class="subheader--metric"># hours w/ more than
+                    <input v-model = "greaterThan" placeholder="number" class = "input">
+                    volunteers</div> 
+                <div class = "container-content"> 
+                    <div class= "metrics-info" >{{getNumHours(false)}} </div>
+                </div>
+            </div>
+
+        <div class = "container">
+            <div class="subheader--calendar">Coverage Map</div>
+                <div class = "container-content">
+                    <div class = "subject-selecter">
+                        <v-select v-model= "selected" :options="topics" @input ="getAvailability"> </v-select>
+                    </div>
+                    <div class = "grid">
+                        <div v-for="(cell, index) in availabilityTable"
+                        :key = "`${index}`">
+                            <div v-bind:class = "[{'cell--header': typeof(cell) == 'string',
+                            'cell--data': typeof(cell) != 'string'}]" 
+                            :style = " { '--rgb': getColor(cell), '--hour': getGradient(cell)}">
+                                {{cell}}
+                            </div>
+                        </div>
                     </div>
                 </div>
-       </div>
+        </div>
     </div>
-    </div>
-    <div v-if="msg !== ''">{{ msg }}</div>
 </div>
-
-
-
-        </template>
+</template>
 
 <script>
 import UserService from '@/services/UserService'
@@ -35,6 +54,8 @@ export default{
 
         return {
             msg: '',
+            lessThan : null,
+            greaterThan: null,
             topics: ['algebra', 'applications', 'biology', 'calculus', 'chemistry', 'esl', 'essays', 'geometry', 'planning', 'precalculus', 'trigonometry'],
             selected : 'algebra',
             maxVolunteers : 0, //stores the max volunteers this week
@@ -44,84 +65,155 @@ export default{
         },
  
     created () {
-       this.calendarToArray(this.selected)
+       this.getAvailability(this.selected)
     },
 
     methods: {
-        getGradient(hour){
-            let gradient = ((hour/(this.maxVolunteers - this.minVolunteers)) * (70) + 20) + '%'
+        getNumHours(lessThan){
+            let compareWith = null
+            if(lessThan){
+                 compareWith = this.lessThan
+            }
+            else{ 
+                compareWith = this.greaterThan
+            }
+            let totalHours = 0
+            if(this.availabilityTable && this.availabilityTable.length != 0){
+                this.availabilityTable.forEach(currentValue => {
+                    if(compareWith && (typeof(currentValue)=== 'number')){
+                       if(this.lessThan && currentValue < compareWith ||
+                        this.greaterThan && currentValue > compareWith){
+                            totalHours++ 
+                        }
+                    } 
+                })
+            }
+            return totalHours
+        },
+
+        getColor(cell) {
+            if(this.greaterThan && cell > this.greaterThan && 
+            this.lessThan && cell < this.lessThan){
+                return '255, 0, 255'
+            }
+            else if(this.lessThan && cell < this.lessThan){
+                return '255, 255, 0'
+            }
+            else if(this.greaterThan && cell > this.greaterThan){
+                return '0, 255, 0'
+            }
+            else{
+                return '255, 0, 0'
+            }
+        },
+        getGradient(cell){
+            let gradient = ((cell/(this.maxVolunteers - this.minVolunteers)) * (70) + 20) + '%'
             return gradient
         },
 
-    calendarToArray(certifiedSubject){
-        this.maxVolunteers = 0
-        this.minVolunteers = 0
+    getAvailability(certifiedSubject){
         UserService.getVolunteersAvailability(this, certifiedSubject).then(availability =>{
             this.minVolunteers = availability.min
             this.maxVolunteers = availability.max
-            this.availabilityTable = availability.table
+            this.availabilityTable = availability.table.flat()
             return this.availabilityTable
         }).catch(err => {
           this.msg = err.message
         })
-    },
- 
-    convertTimeToIndex(time){ //USE CALENDAR SERVICE 24 HOUR TIME
-        let newTime 
-        if (time=='12a'){
-            newTime = 0}
-        else if (time=='12p'){
-            newTime = 12 }
-        else if (time.slice(-1)=='p'){
-            newTime = parseInt(time.slice(0,-1))+12 
-        }
-        else{
-        newTime = (time.slice(0,-1))
-        }
-        return parseInt(newTime, 10)
-        }
+        },
     }
 }
     
 </script>
 
 <style lang="scss" scoped>
-:root, [data-theme="default"] {
-    --test: ".3";
+
+.wrap-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
 }
-.calendar{
-    display: grid;
-    max-width: 560px;
-  margin: 20px auto;
-  grid-template-columns: repeat(8, auto);
-}
-.header{
-    grid-area: header;
-}
-.day{
+.container {
     display: flex;
-    justify-content: space-evenly;
+    flex-direction: column;
+    margin: 1vw;
+     &--metric {
+        @extend .container;
+        width: 170px;
+    }
 }
-.hour{
-    flex-grow: 1;
-    flex-shrink: 1;
-    flex-basis: 0;
+
+.grid{
+    display: grid;
+    margin: 20px;
+    grid-auto-flow: column;
+    grid-template-columns: 30px repeat(7, 1fr);
+    grid-template-rows: repeat(25, 1fr);
+    grid-gap: 2px;
+
 }
-.hour {
-  background-color: hsl(0, 100%, var(--hour));
+.metrics-info {
+    font-size: 40px;
+}
+
+.cell {
   color: gray;
-  padding: 5px;
-  box-shadow: 
-    -1px -1px gray, 
-    inset -1px -1px 0 0 gray;
+    &--header {
+        @extend .cell;
+        font: bold;
+        overflow: hidden;
+    }
+    &--data {
+        @extend .cell;
+        background-color: rgba(var(--rgb), (var(--hour)));
+    }
 }
 
-.test {
-    background: hsla(120, 30%, 20%, var(--degs));
-}
-
-.choose {
+.input {
+    width: 100px;
+    font-size: 15px;
     text-align: center;
+}
+
+.subheader {
+  font-weight: 600;
+  justify-content: center;
+  align-items: center;
+  background-color: #e3f2fd;
+  font-size: 20px;
+  padding: 10px;
+    &--metric{
+      @extend .subheader;
+      height: 150px;
+  }
+    &--calendar{
+      @extend .subheader;
+      height: 50px;
+  }
+    &--input {
+  background-color: #e3f2fd;
+  }
+}
+
+.container-content {
+  background-color: #f0f8fd;
+  padding: 1vw;
+}
+
+.subject-selecter {
+  background-color: white;
+}
+
+.header{
+  display: flex;
+  padding: 30px;
+  margin: 0;
+  font-size: 24px;
+  border-bottom: 0.5px solid #cccccf;
+  align-items: center;
+  justify-content: space-between;
+  font-weight: 600;
+  color: #343440;;
 }
 
 </style>
