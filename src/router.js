9015f2115154a88bb5269a30bac976ab02017abf
Jack Keating <keatjm@gmail.com>
2019-08-13 16:13:37 -0500

    In local development, link to Edu Admin panel sends browser from :8080 to :3000

--- src/router.js
+++ src/router.js
@@ -184,6 +184,14 @@ const routes = [
   {
     path: "/edu", // TODO: make this be "/admin/edu"
     component: () => {
+      if (process.env.NODE_ENV === "development") {
+        // The EDU admin route is rendered server-side with Express.js, so in local development
+        // we need to move to port 3000 (away from Vue's dev server on port 8080)
+        window.location.href = "http://localhost:3000/edu";
+        return;
+      }
+
+      // In non-development environments, we can simply use a relative link
       window.location.href = "/edu";
     }
   }
