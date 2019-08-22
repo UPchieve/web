4076e3e75c44237ec9df912bda83f25e522b42c9
Justin Moody <25038653+moody@users.noreply.github.com>
2019-08-22 17:36:52 -0400

    Figma design system (#207)
    * organize scss + add font-category mixin
    
    * add `validate-map-key` + improve `child-spacing`
    
    * add upchieve icon font & component
    
    * add LargeButton component
    
    * add ButtonTemplate + HyperlinkButton
    
    * add TabButton component
    
    * add TextField component
    
    * add DropdownList component
    
    * add CheckBox component
    
    * install 'Work Sans' font
    
    * add RadioButton component
    
    * add subject icons
    
    * run lint
    
    * rem subfolders

---
+++ src/components/ButtonTemplate.vue
@@ -0,0 +1,54 @@
+<template>
+  <router-link v-if="routeTo" tag="button" :to="routeTo" class="ButtonTemplate">
+    <div>
+      <slot />
+    </div>
+    <upchieve-icon
+      v-if="primary"
+      icon="arrow"
+      color="inherit"
+      class="ButtonTemplate-icon"
+    />
+  </router-link>
+
+  <button v-else class="ButtonTemplate">
+    <div>
+      <slot />
+    </div>
+    <upchieve-icon
+      v-if="primary"
+      icon="arrow"
+      color="inherit"
+      class="ButtonTemplate-icon"
+    />
+  </button>
+</template>
+
+<script>
+import UpchieveIcon from "@/components/UpchieveIcon";
+
+export default {
+  components: { UpchieveIcon },
+  props: {
+    primary: Boolean,
+    routeTo: String
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.ButtonTemplate {
+  @include font-category("button");
+  @include flex-container(row, center, center);
+
+  background: none;
+  border: none;
+  margin: 0;
+  padding: 0;
+}
+
+.ButtonTemplate-icon {
+  margin-top: 2px; // nudge down
+  margin-left: 8px; // space between text
+}
+</style>
