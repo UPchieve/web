7ca4a9b9ff4da546b9751b29eaa0f1e50d72232f
Jack Keating <keatjm@gmail.com>
2019-08-27 17:01:25 -0400

    remove volunteer resources link

--- src/components/App/AppSidebar/SidebarLinks.vue
+++ src/components/App/AppSidebar/SidebarLinks.vue
@@ -30,7 +30,6 @@
       />
       <sidebar-link v-if="isAdmin" to="/admin" icon="folder" text="Admin" />
       <sidebar-link to="/profile" icon="portrait" text="Profile" />
-      <sidebar-link to="/resources" icon="folder" text="Resources" />
     </template>
 
     <template v-else>
