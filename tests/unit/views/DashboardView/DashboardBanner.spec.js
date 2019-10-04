9b74a6c1a3ce69b0d4bffe1f64e1aba325d6c4b1
Jack Keating <keatjm@gmail.com>
2019-10-04 15:59:41 -0400

    fix unit tests

--- tests/unit/views/DashboardView/DashboardBanner.spec.js
+++ tests/unit/views/DashboardView/DashboardBanner.spec.js
@@ -33,7 +33,6 @@ describe("DashboardView", () => {
   describe("layout", () => {
     test("mobile", () => {
       const wrapper = getWrapper(true);
-      wrapper.setData({ name: "Tester" });
       expect(wrapper.classes("DashboardBanner")).toBe(true);
 
       const greeting = wrapper.find(".DashboardBanner-greeting");
@@ -42,15 +41,10 @@ describe("DashboardView", () => {
 
       const banner = wrapper.find(".DashboardBanner-banner");
       expect(banner.exists()).toBe(true);
-
-      // const tourButton = banner.find(LargeButton);
-      // expect(tourButton.exists()).toBe(true);
-      // expect(tourButton.text()).toBe("Take a tour");
     });
 
     test("desktop", () => {
       const wrapper = getWrapper(false);
-      wrapper.setData({ name: "Tester" });
       expect(wrapper.classes("DashboardBanner")).toBe(true);
 
       const banner = wrapper.find(".DashboardBanner-banner");
@@ -59,10 +53,6 @@ describe("DashboardView", () => {
       const greeting = banner.find(".DashboardBanner-greeting");
       expect(greeting.exists()).toBe(true);
       expect(greeting.text()).toBe("Hello, Tester!");
-
-      // const tourButton = banner.find(LargeButton);
-      // expect(tourButton.exists()).toBe(true);
-      // expect(tourButton.text()).toBe("Take a tour");
     });
   });
 });
