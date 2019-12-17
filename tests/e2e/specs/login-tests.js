770fd02f6904f6ddf887e2097c6a61b53edc0c84
Stewart <stewartpwallace@gmail.com>
2019-12-17 15:01:30 -0500

    Ran lint

--- tests/e2e/specs/login-tests.js
+++ tests/e2e/specs/login-tests.js
@@ -1,21 +1,19 @@
-
 /**
  *
- * @file Tests user authenication (login/ logout) and failed attempts for both 
- * 
+ * @file Tests user authenication (login/ logout) and failed attempts for both
+ *
  */
 
-/** 
+/**
  * @summary Tests authenicating a volunteer and student successfully
  */
- 
+
 describe("Successful Log in and log out", () => {
   before(() => {
     cy.fixture("users/student1").as("student");
     cy.fixture("users/Volunteer1").as("volunteer");
-
   });
-  
+
   describe("Authenicate a student logging in and out", () => {
     it("Should log in successfully", function() {
       cy.visit("/");
@@ -32,16 +30,13 @@ describe("Successful Log in and log out", () => {
 
       cy.location("pathname").should("eq", "/dashboard");
     });
-  
 
     it("Should log out successfully", () => {
-      cy.contains("Log out")
-        .click();
+      cy.contains("Log out").click();
 
       cy.location("pathname").should("eq", "/logout");
     });
   });
-
 
   describe("Authenicate a volunteer logging in and out", () => {
     it("Should log in successfully", function() {
@@ -59,67 +54,63 @@ describe("Successful Log in and log out", () => {
 
       cy.location("pathname").should("eq", "/dashboard");
     });
-  
 
     it("Should log out successfully", () => {
-      cy.contains("Log out")
-        .click();
+      cy.contains("Log out").click();
 
       cy.location("pathname").should("eq", "/logout");
     });
   });
 });
 
-
-/** 
+/**
  * @summary Tests failing to log in
  */
- 
+
 describe("Fail logging in", () => {
   beforeEach(() => {
     cy.fixture("users/student1").as("student");
   });
 
-  it("Use incorrect email", function() { 
-   cy.server()
-   cy.route('/api/user').as('userAPI')
-   cy.visit("/login");
+  it("Use incorrect email", function() {
+    cy.server();
+    cy.route("/api/user").as("userAPI");
+    cy.visit("/login");
 
-   cy.get("#inputEmail")
+    cy.get("#inputEmail")
       .type("fake@email.com")
       .should("have.value", "fake@email.com");
 
-   cy.get("#inputPassword")
+    cy.get("#inputPassword")
       .type(this.student.password)
       .should("have.value", this.student.password);
 
-   cy.get("button[type=submit]").click();
-   
-   cy.wait('@userAPI').then(function(xhr){
-     const response = xhr.status
-     expect(response).to.equal(401)
-   })
+    cy.get("button[type=submit]").click();
+
+    cy.wait("@userAPI").then(function(xhr) {
+      const response = xhr.status;
+      expect(response).to.equal(401);
+    });
   });
 
-  it("Use incorrect password", function() { 
-   cy.server()
-   cy.route('/api/user').as('userAPI')
-   cy.visit("/login");
+  it("Use incorrect password", function() {
+    cy.server();
+    cy.route("/api/user").as("userAPI");
+    cy.visit("/login");
 
-   cy.get("#inputEmail")
+    cy.get("#inputEmail")
       .type(this.student.email)
       .should("have.value", this.student.email);
 
-   cy.get("#inputPassword")
+    cy.get("#inputPassword")
       .type("fakepassword")
       .should("have.value", "fakepassword");
 
-   cy.get("button[type=submit]").click();
-   
-   cy.wait('@userAPI').then(function(xhr){
-     const response = xhr.status
-     expect(response).to.equal(401)
-   })
+    cy.get("button[type=submit]").click();
+
+    cy.wait("@userAPI").then(function(xhr) {
+      const response = xhr.status;
+      expect(response).to.equal(401);
+    });
   });
 });
-
