591ccdd6f00eb02a1e7f943529c33e1e113af040
Jack Keating <keatjm@gmail.com>
2020-01-03 17:05:04 -0600

    remove unused calendar methods

--- src/services/CalendarService.js
+++ src/services/CalendarService.js
@@ -1,23 +1,8 @@
 import NetworkService from "./NetworkService";
 
 export default {
-  initAvailability(context, userid) {
-    return NetworkService.initAvailability(context, { userid });
-  },
-  getAvailability(context, userid) {
-    return NetworkService.getAvailability(context, { userid }).then(res => {
-      const availability = { ...res.data.availability };
-      return availability;
-    });
-  },
   updateAvailability(context, userid, availability) {
     return NetworkService.updateAvailability(context, { userid, availability });
-  },
-  getTimezone(context, userid) {
-    return NetworkService.getTimezone(context, { userid }).then(res => {
-      const tz = res.data.tz;
-      return tz;
-    });
   },
   updateTimezone(context, userid, tz) {
     return NetworkService.updateTimezone(context, { userid, tz });
