32ff26f9574be50f67edefc76f1b4c3d0febc574
David Nisson <d.m.nisson@gmail.com>
2020-01-16 11:40:52 -0800

    remove userid from calendar and quiz request bodies

--- src/services/CalendarService.js
+++ src/services/CalendarService.js
@@ -1,12 +1,12 @@
 import NetworkService from "./NetworkService";
 
 export default {
-  updateAvailability(context, userid, availability) {
+  updateAvailability(context, availability) {
     context.$store.dispatch("user/updateAvailability", availability);
-    return NetworkService.updateAvailability(context, { userid, availability });
+    return NetworkService.updateAvailability(context, { availability });
   },
-  updateTimezone(context, userid, tz) {
+  updateTimezone(context, tz) {
     context.$store.dispatch("user/updateTimezone", tz);
-    return NetworkService.updateTimezone(context, { userid, tz });
+    return NetworkService.updateTimezone(context, { tz });
   }
 };
