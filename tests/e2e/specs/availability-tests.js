44cbb1d39349112f9d5b860339d4daee78ac43fb
Jack Keating <keatjm@gmail.com>
2020-02-27 19:00:13 -0600

    Add tests modifying availability and timezone

--- tests/e2e/specs/availability-tests.js
+++ tests/e2e/specs/availability-tests.js
@@ -1,18 +1,122 @@
+const DEFAULT_TIMEZONE = "America/New_York";
+const SELECTED_TIMES_SELECTOR = ".timeOfDay input:checked";
+const THURSDAY_8AM_SELECTOR =
+  ".dayTime div:nth-of-type(5) .times .timeOfDay:nth-of-type(8) input[type='checkbox']";
+
 describe("Volunteer availability", () => {
   before(function() {
     cy.fixture("users/Volunteer1").as("volunteer");
   });
 
   describe("Modify availability calendar", () => {
+    before(function() {
+      cy.login(this.volunteer);
+
+      const clearScheduleUrl = `${Cypress.env(
+        "SERVER_ROOT"
+      )}/api/calendar/clear`;
+
+      cy.request({
+        method: "POST",
+        url: clearScheduleUrl,
+        body: {
+          tz: DEFAULT_TIMEZONE
+        }
+      });
+    });
+
     beforeEach(function() {
       cy.login(this.volunteer);
     });
 
-    /**
-     * TODO:
-     * Get first unchecked time (unless all selected)
-     * cy.get('.timeOfDay input:not(:checked)').first()
-     */
+    it("Should modify but not save the calendar", function() {
+      cy.visit("/calendar");
+      cy.location("pathname").should("eq", "/calendar");
+
+      cy.get(".dayTimeContainer")
+        .find(SELECTED_TIMES_SELECTOR)
+        .should("have.length", 0);
+
+      cy.get(THURSDAY_8AM_SELECTOR)
+        .should("not.be.checked")
+        .check()
+        .should("be.checked");
+
+      cy.get(".dayTimeContainer")
+        .find(SELECTED_TIMES_SELECTOR)
+        .should("have.length", 1);
+
+      cy.reload();
+
+      cy.get(".dayTimeContainer")
+        .find(SELECTED_TIMES_SELECTOR)
+        .should("have.length", 0);
+    });
+
+    it("Should modify and then save the calendar", function() {
+      cy.visit("/calendar");
+      cy.location("pathname").should("eq", "/calendar");
+
+      cy.get(".dayTimeContainer")
+        .find(SELECTED_TIMES_SELECTOR)
+        .should("have.length", 0);
+
+      cy.get(THURSDAY_8AM_SELECTOR)
+        .should("not.be.checked")
+        .check()
+        .should("be.checked");
+
+      cy.get(".dayTimeContainer")
+        .find(SELECTED_TIMES_SELECTOR)
+        .should("have.length", 1);
+
+      cy.get(".save-button")
+        .should("contain.text", "Save")
+        .click()
+        .should("contain.text", "Saved");
+
+      cy.reload();
+
+      cy.get(".dayTimeContainer")
+        .find(SELECTED_TIMES_SELECTOR)
+        .should("have.length", 1);
+    });
+
+    it("Should modify and then save the timezone", function() {
+      cy.visit("/calendar");
+      cy.location("pathname").should("eq", "/calendar");
+
+      cy.get(THURSDAY_8AM_SELECTOR).should("be.checked");
+
+      cy.get(".dayTimeContainer")
+        .find(SELECTED_TIMES_SELECTOR)
+        .should("have.length", 1);
+
+      const NEW_TIMEZONE = "Antarctica/Troll";
+
+      cy.get(".tz-selector-container select")
+        .should("have.value", DEFAULT_TIMEZONE)
+        .select(NEW_TIMEZONE)
+        .should("have.value", NEW_TIMEZONE);
+
+      cy.get(".save-button")
+        .should("contain.text", "Save")
+        .click()
+        .should("contain.text", "Saved");
+
+      cy.reload();
+
+      cy.get(".tz-selector-container select").should(
+        "have.value",
+        NEW_TIMEZONE
+      );
+
+      cy.get(THURSDAY_8AM_SELECTOR).should("be.checked");
+
+      cy.get(".dayTimeContainer")
+        .find(SELECTED_TIMES_SELECTOR)
+        .should("have.length", 1);
+    });
 
     it("Should display accurate availability on dashboard", function() {
       cy.visit("/dashboard");
@@ -34,7 +138,7 @@ describe("Volunteer availability", () => {
           cy.visit("/calendar");
 
           cy.get(".dayTimeContainer")
-            .find(".timeOfDay input:checked")
+            .find(SELECTED_TIMES_SELECTOR)
             .should("have.length", hoursSelected);
         });
     });
