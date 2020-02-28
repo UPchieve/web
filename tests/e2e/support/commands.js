2d81f98e0d7705c1c66beefdca0c6c3e7cd241e1
Trey Stevens <trey.stevens@upchieve.org>
2020-02-28 16:30:17 -0500

    cypress commands to getSessionId from url and clearSession

--- tests/e2e/support/commands.js
+++ tests/e2e/support/commands.js
@@ -53,3 +53,24 @@ Cypress.Commands.add("deleteUserByEmail", email => {
     }
   });
 });
+
+Cypress.Commands.add("getSessionId", url => {
+  let index = url.length - 1;
+
+  while (url[index - 1] !== "/") {
+    index--;
+  }
+  const sessionId = url.slice(index, url.length);
+
+  return sessionId;
+});
+
+Cypress.Commands.add("clearSession", sessionId => {
+  cy.request({
+    method: "POST",
+    url: `${API_ROOT}/session/end`,
+    body: {
+      sessionId
+    }
+  });
+});
