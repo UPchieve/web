2004d23f6123830c045cfa8d0cd853da540331e6
David Nisson <d.m.nisson@gmail.com>
2020-02-28 21:38:45 -0800

    move fixtures into beforeEach hook

--- tests/e2e/specs/training-tests.js
+++ tests/e2e/specs/training-tests.js
@@ -1,13 +1,13 @@
 describe("Training quizzes", function() {
-  before(function() {
+  beforeEach(function() {
     cy.fixture("users/volunteer1").as("adminVolunteer");
     cy.fixture("users/volunteer3").as("untrainedVolunteer");
-  });
-
-  beforeEach(function() {
-    cy.login(this.untrainedVolunteer);
 
     const getProfileUrl = `${Cypress.env("SERVER_ROOT")}/api/user`;
+
+    cy.get("@untrainedVolunteer").then(untrainedVolunteer =>
+      cy.login(untrainedVolunteer)
+    );
 
     cy.request({
       url: getProfileUrl
@@ -19,32 +19,55 @@ describe("Training quizzes", function() {
           user.certifications.algebra.passed ||
           user.certifications.algebra.tries === 3
         ) {
+          cy.get("@adminVolunteer").then(adminVolunteer =>
+            cy.login(adminVolunteer)
+          );
+
+          cy.get("@untrainedVolunteer")
+            .its("email")
+            .then(email => cy.deleteUserByEmail(email));
+
           cy.logout();
 
-          cy.login(this.adminVolunteer);
-          cy.deleteUserByEmail(this.untrainedVolunteer.email);
+          cy.get("@untrainedVolunteer").then(untrainedVolunteer => {
+            const userObj = Object.assign({}, untrainedVolunteer);
 
-          cy.getVolunteerCodes()
-            .then(codes => {
-              cy.logout();
+            cy.get("@adminVolunteer").then(adminVolunteer =>
+              cy.login(adminVolunteer)
+            );
 
-            const userObj = Object.assign({}, this.untrainedVolunteer);
-            userObj.code = codes[0];
+            cy.getVolunteerCodes().then(codes => {
+              userObj.code = codes[0];
 
-              return cy.createUser(userObj);
-            })
-            .then(() => {
-              cy.login(this.adminVolunteer);
-              cy.getVerificationToken().as("token");
-              cy.logout();
+              cy.createUser(userObj);
+            });
+          });
 
-              const verifyUserUrl = `${Cypress.env(
-                "SERVER_ROOT"
-              )}/api/verify/confirm?userid=${user._id}`;
+          cy.request({
+            url: getProfileUrl
+          })
+            .its("body.user")
+            .then(user => {
+              cy.get("@adminVolunteer").then(adminVolunteer =>
+                cy.login(adminVolunteer)
+              );
 
-              cy.login(this.untrainedVolunteer);
-              cy.request({
-                url: verifyUserUrl
+              cy.getVerificationToken(user._id).then(token => {
+                cy.logout();
+
+                const verifyUserUrl = `${Cypress.env(
+                  "SERVER_ROOT"
+                )}/api/verify/confirm?userid=${user._id}`;
+
+                cy.get("@untrainedVolunteer").then(untrainedVolunteer =>
+                  cy.login(untrainedVolunteer)
+                );
+
+                cy.request({
+                  method: "POST",
+                  url: verifyUserUrl,
+                  body: { token }
+                });
               });
             });
         }
@@ -72,42 +95,69 @@ describe("Training quizzes", function() {
       .children(".test-container")
       .children(".test-label")
       .click();
-
-    cy.wait("@getQuestions")
-      .its("response.body.questions")
-      .as("questions");
   });
 
   it("Should pass algebra quiz", function() {
-    cy.get("button.start").click();
+    cy.wait("@getQuestions")
+      .its("response.body.questions")
+      .then(questions => {
+        cy.logout();
+        cy.login(this.adminVolunteer);
+        const categoryQuestionsUrl = `${Cypress.env(
+          "SERVER_ROOT"
+        )}/edu/categoryquestions`;
 
-    cy.logout();
-    cy.login(this.adminVolunteer);
-    const categoryQuestionsUrl = `${Cypress.env(
-      "SERVER_ROOT"
-    )}/edu/categoryquestions`;
+        cy.request({
+          method: "POST",
+          url: categoryQuestionsUrl,
+          body: { category: "algebra" }
+        })
+          .its("body.questions")
+          .as("categoryQuestions");
+        cy.logout();
+        cy.login(this.untrainedVolunteer);
 
-    cy.request({
-      method: "POST",
-      url: categoryQuestionsUrl,
-      body: { category: "algebra" }
-    })
-      .its("body.questions")
-      .as("categoryQuestions");
-    cy.logout();
-    cy.login(this.untrainedVolunteer);
+        cy.get("@categoryQuestions").then(categoryQuestions => {
+          cy.get("button.start").click();
 
-    Promise.all([cy.get("@questions"), cy.get("@categoryQuestions")]).then(
-      results => {
-        const questions = results[0];
-        const categoryQuestions = results[1];
+          questions.forEach((question, i) => {
+            cy.log(JSON.stringify(categoryQuestions));
+            cy.log(JSON.stringify(question));
+            const correctAnswer = categoryQuestions.filter(
+              cq => cq._id === question._id
+            )[0].correctAnswer;
+
+            cy.get(`input[type=radio][value=${correctAnswer}]`).click();
+
+            if (i < questions.length - 1) {
+              cy.get("button[type=next]").click();
+            } else {
+              cy.get("button[type=submit]").click();
+            }
+          });
+
+          cy.wait("@score")
+            .its("response.body")
+            .then(data => {
+              cy.get(".passed").should("contain", "passed");
+
+              cy.get(".score").should("contain", data.score.toString());
+            });
+        });
+      });
+  });
+
+  it("Should fail algebra quiz", function() {
+    cy.wait("@getQuestions")
+      .its("response.body.questions")
+      .then(questions => {
+        cy.get("button.start").click();
 
         questions.forEach((question, i) => {
-          const correctAnswer = categoryQuestions.filter(
-            cq => cq._id === question._id
-          )[0].correctAnswer;
-
-          cy.get(`input[type=radio][value=${correctAnswer}]`).click();
+          cy.get("form.possibleAnswers > div:first-child")
+            .children(".options")
+            .children("input[type=radio]")
+            .click();
 
           if (i < questions.length - 1) {
             cy.get("button[type=next]").click();
@@ -119,38 +169,10 @@ describe("Training quizzes", function() {
         cy.wait("@score")
           .its("response.body")
           .then(data => {
-            cy.get(".passed").should("contain", "passed");
+            cy.get(".passed").should("contain", "failed");
 
             cy.get(".score").should("contain", data.score.toString());
           });
-      }
-    );
-  });
-
-  it("Should fail algebra quiz", function() {
-    cy.get("button.start").click();
-
-    cy.get("@questions").then(questions => {
-      questions.forEach((question, i) => {
-        cy.get("form.possibleAnswers > div:first-child")
-          .children(".options")
-          .children("input[type=radio]")
-          .click();
-
-        if (i < questions.length - 1) {
-          cy.get("button[type=next]").click();
-        } else {
-          cy.get("button[type=submit]").click();
-        }
       });
-
-      cy.wait("@score")
-        .its("response.body")
-        .then(data => {
-          cy.get(".passed").should("contain", "failed");
-
-          cy.get(".score").should("contain", data.score.toString());
-        });
-    });
   });
 });
