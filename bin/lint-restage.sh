6dbc86e8ac2357c8137dbd5d125f53af2385f0bb
David Nisson <d.m.nisson@gmail.com>
2020-03-03 14:13:20 -0800

    pre- and post-commit husky hooks for automatic linting

---
+++ bin/lint-restage.sh
@@ -0,0 +1,6 @@
+# Lints staged files by stashing any unstaged changes, then linting, then popping the
+# stash
+# See https://codeinthehole.com/tips/tips-for-using-a-git-pre-commit-hook/
+git stash -q --keep-index
+npm run lint
+git add -u
