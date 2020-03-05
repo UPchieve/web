5ef7187eafa3db59e10709e3a1d79198bd664b01
David Nisson <d.m.nisson@gmail.com>
2020-03-05 12:30:02 -0800

    restore unstaged changes if error occurs

--- bin/lint-restage.sh
+++ bin/lint-restage.sh
@@ -2,4 +2,17 @@
 # stash
 # See https://codeinthehole.com/tips/tips-for-using-a-git-pre-commit-hook/
 git stash -q --keep-index
-npm run lint && git add -u
+npm run lint
+RESULT=$?
+
+# stage the automatic fixes
+git add -u
+
+# restore original unstaged files if errors occurred
+if [ $RESULT -ne 0 ]; then
+  git stash pop -q
+fi
+
+# ensure commit fails if lint could not fix everything
+[ $RESULT -ne 0 ] && exit 1
+exit 0
