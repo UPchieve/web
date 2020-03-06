26b2cb661da6b1a5a120281b12468a77706858e9
Trey Stevens <trey.stevens@upchieve.org>
2020-03-06 13:26:16 -0500

    removeViewBox false during vue compile

--- vue.config.js
+++ vue.config.js
@@ -17,6 +17,13 @@ module.exports = {
 
     const svgRule = config.module.rule("svg");
     svgRule.uses.clear();
-    svgRule.use("vue-svg-loader").loader("vue-svg-loader");
+    svgRule
+      .use("vue-svg-loader")
+      .loader("vue-svg-loader")
+      .options({
+        svgo: {
+          plugins: [{ removeViewBox: false }]
+        }
+      });
   }
 };
