de23f4c6f630898152eb9d6b3ce27f7489b26681
Trey Stevens <trey.stevens@upchieve.org>
2020-03-13 08:28:30 -0400

    view for review material pdfs

---
+++ src/views/ReviewMaterialsView.vue
@@ -0,0 +1,243 @@
+<template>
+  <div class="wrapper">
+    <h1 class="header-title">{{ category }} Review Materials</h1>
+    <div class="review-materials">
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        class="review-materials__link"
+        v-for="review in reviewMaterials"
+        :href="review.pdf"
+        :key="review.title"
+      >
+        <div class="review-materials__content">
+          <div class="image-overlay"></div>
+          <img :src="review.image" class="review-materials__image" />
+          <span class="review-materials__title">{{ review.title }}</span>
+        </div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+import NetworkService from "@/services/NetworkService";
+
+export default {
+  components: {},
+  data() {
+    return {
+      reviewMaterials: [],
+      category: ""
+    };
+  },
+  mounted() {
+    const { params } = this.$route;
+    const { category } = params;
+    this.category = category;
+    this.getCategoryMaterials();
+    this.getReviewMaterials();
+  },
+  methods: {
+    getCategoryMaterials() {
+      const LONG_REVIEW_TEXT = "Long Review Guide";
+      const SHORT_REVIEW_TEXT = "Short Review Guide";
+
+      switch (this.category) {
+        case "algebra":
+          this.reviewMaterials = [
+            {
+              title: "Concept Review",
+              pdf: require(`../assets/review_materials/algebra/algebra-concept-review.pdf`),
+              image: require(`../assets/review_materials/algebra/algebra-concept-review.png`)
+            },
+            {
+              title: "Topics & Resources",
+              pdf: require(`../assets/review_materials/algebra/algebra-topics-and-resources.pdf`),
+              image: require(`../assets/review_materials/algebra/algebra-topics-and-resources.png`)
+            },
+            {
+              title: "Overall Guide",
+              pdf: require(`../assets/review_materials/algebra/algebra-overall-guide.pdf`),
+              image: require(`../assets/review_materials/algebra/algebra-overall-guide.png`)
+            }
+          ];
+          break;
+        case "geometry":
+          this.reviewMaterials = [
+            {
+              title: LONG_REVIEW_TEXT,
+              pdf: require(`../assets/review_materials/geometry/geometry-long-guide.pdf`),
+              image: require(`../assets/review_materials/geometry/geometry-long-guide.png`)
+            },
+            {
+              title: SHORT_REVIEW_TEXT,
+              pdf: require(`../assets/review_materials/geometry/geometry-short-guide.pdf`),
+              image: require(`../assets/review_materials/geometry/geometry-short-guide.png`)
+            }
+          ];
+          break;
+        case "trigonometry":
+          this.reviewMaterials = [
+            {
+              title: LONG_REVIEW_TEXT,
+              pdf: require(`../assets/review_materials/trigonometry/trigonometry-long-guide.pdf`),
+              image: require(`../assets/review_materials/trigonometry/trigonometry-long-guide.png`)
+            },
+            {
+              title: SHORT_REVIEW_TEXT,
+              pdf: require(`../assets/review_materials/trigonometry/trigonometry-short-guide.pdf`),
+              image: require(`../assets/review_materials/trigonometry/trigonometry-short-guide.png`)
+            }
+          ];
+          break;
+        case "precalculus":
+          this.reviewMaterials = [
+            {
+              title: LONG_REVIEW_TEXT,
+              pdf: require(`../assets/review_materials/precalculus/precalculus-long-guide.pdf`),
+              image: require(`../assets/review_materials/precalculus/precalculus-long-guide.png`)
+            },
+            {
+              title: SHORT_REVIEW_TEXT,
+              pdf: require(`../assets/review_materials/precalculus/precalculus-short-guide.pdf`),
+              image: require(`../assets/review_materials/precalculus/precalculus-short-guide.png`)
+            }
+          ];
+          break;
+        case "calculus":
+          this.reviewMaterials = [
+            {
+              title: LONG_REVIEW_TEXT,
+              pdf: require(`../assets/review_materials/calculus/calculus-long-guide.pdf`),
+              image: require(`../assets/review_materials/calculus/calculus-long-guide.png`)
+            },
+            {
+              title: SHORT_REVIEW_TEXT,
+              pdf: require(`../assets/review_materials/calculus/calculus-short-guide.pdf`),
+              image: require(`../assets/review_materials/calculus/calculus-short-guide.png`)
+            }
+          ];
+          break;
+        case "planning":
+          this.reviewMaterials = [
+            {
+              title: "College Planning Guide",
+              pdf: require(`../assets/review_materials/planning/college-planning-guide.pdf`),
+              image: require(`../assets/review_materials/planning/college-planning-guide.png`)
+            }
+          ];
+          break;
+        case "essays":
+          this.reviewMaterials = [
+            {
+              title: "Essay Editing Guide",
+              pdf: require(`../assets/review_materials/essays/essay-editing-guide.pdf`),
+              image: require(`../assets/review_materials/essays/essay-editing-guide.png`)
+            }
+          ];
+          break;
+        case "applications":
+          this.reviewMaterials = [
+            {
+              title: "Application Help Guide",
+              pdf: require(`../assets/review_materials/applications/application-help-guide.pdf`),
+              image: require(`../assets/review_materials/applications/application-help-guide.png`)
+            }
+          ];
+          break;
+        default:
+          break;
+      }
+    },
+    getReviewMaterials() {
+      NetworkService.getReviewMaterials(this, this.category);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+  padding: 40px 40px 80px;
+  margin: 40px 40px 80px;
+  background: #fff;
+  border-radius: 8px;
+  max-width: 800px;
+}
+
+.header-title {
+  text-transform: capitalize;
+  text-align: left;
+  font-size: 24px;
+  font-weight: 500;
+  text-align: left;
+  margin: 0 0 35px 0;
+}
+
+.review-materials {
+  display: flex;
+  flex-flow: row wrap;
+  padding-left: 0;
+
+  &__link {
+    margin-right: 2em;
+    display: block;
+    margin-bottom: 1em;
+  }
+
+  &__content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 200px;
+    background-color: white;
+    border-radius: 5px;
+    border: 1px solid gray;
+    position: relative;
+
+    &:hover .image-overlay {
+      opacity: 0.6;
+    }
+  }
+
+  &__title {
+    padding: 1em 0;
+  }
+
+  &__image {
+    width: 100%;
+    border-top-left-radius: 5px;
+    border-top-right-radius: 5px;
+    display: block;
+  }
+}
+
+.image-overlay {
+  width: 100%;
+  position: absolute;
+  z-index: 5;
+  opacity: 0;
+  left: 0;
+  top: 0;
+  height: inherit;
+  background-image: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0.4) 0%,
+    rgba(0, 0, 0, 0) 100%
+  );
+  height: 140px;
+}
+
+@media screen and (max-width: 375px) {
+  .review-materials {
+    &__link {
+      margin-right: 0;
+    }
+
+    &__content {
+      width: initial;
+    }
+  }
+}
+</style>
