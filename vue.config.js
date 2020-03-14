8e52113cdf0309ddfb36dbd81c3da9dc914bd57c
Trey Stevens <trey.stevens@upchieve.org>
2020-03-14 17:39:29 -0400

    removed inline and external importing

--- vue.config.js
+++ vue.config.js
@@ -19,19 +19,12 @@ module.exports = {
     const svgRule = config.module.rule("svg");
     svgRule.uses.clear();
     svgRule
-      .oneOf("inline")
-      .resourceQuery(/inline/)
       .use("vue-svg-loader")
       .loader("vue-svg-loader")
       .options({
         svgo: {
           plugins: [{ removeViewBox: false }]
         }
-      })
-      .end()
-      .end()
-      .oneOf("external")
-      .use("file-loader")
-      .loader("file-loader");
+      });
   }
 };
