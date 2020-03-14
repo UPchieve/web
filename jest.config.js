a5b0982e2e4e0de3b8b8fd7ee948aa6003e05d54
Trey Stevens <trey.stevens@upchieve.org>
2020-03-14 17:39:45 -0400

    removed transform svg to component from jest config

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,9 @@ module.exports = {
   moduleFileExtensions: ["js", "jsx", "json", "vue"],
   transform: {
     "^.+\\.vue$": "vue-jest",
-    ".+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$":
+    ".+\\.(css|styl|less|sass|scss|png|jpg|svg|ttf|woff|woff2)$":
       "jest-transform-stub",
-    "^.+\\.jsx?$": "babel-jest",
-    "^.+\\.svg$": "<rootDir>/src/utils/svg-transform.js"
+    "^.+\\.jsx?$": "babel-jest"
   },
   transformIgnorePatterns: ["/node_modules/"],
   moduleNameMapper: {
