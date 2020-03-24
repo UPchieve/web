3225d3ea13d53a3930af23d00e79bc8e65d312a0
Trey <35900194+treykris@users.noreply.github.com>
2020-03-24 12:52:18 -0400

    Quiz clean up (#355)
    * use LoaderMessage component for loading messages
    
    * placed removed quizLoading conditional back
    
    * encapsulate different quiz views into components
    
    * updated styles for mobile viewing
    
    * updated quiz results copy and styling
    
    * removed width on progress bar
    
    * style updates
    
    * test > quiz - for consistency
    
    * added loader for getting quiz results & updated styling
    
    * updated copy and styling
    
    * lint fix
    
    * added the amount of tries copy back
    
    * removed unused class names
    
    * message for exceeded # of tries
    
    Co-authored-by: Trey Stevens <trey.stevens@upchieve.org>

---
+++ src/views/QuizView/QuizReview.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="review">
+    <div
+      v-for="(question, index) in questionsReview"
+      :key="`question-${index}`"
+      class="question"
+    >
+      <div class="question-number">Question {{ index + 1 }}</div>
+      <br />
+      <div class="question-text">{{ question.questionText }}</div>
+      <div :style="question.imageStyle" class="question-image" />
+      <div class="possible-answers">
+        <div
+          v-for="(answer, index) in question.possibleAnswers"
+          :key="`answer-${index}`"
+          :id="'answer-' + answer.val"
+        >
+          {{ answer.val }}. {{ answer.txt }}
+        </div>
+      </div>
+      <div class="userAnswer">Your answer: {{ question.userAnswer }}</div>
+      <div class="correctAnswer">
+        Correct answer: {{ question.correctAnswer }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import TrainingService from "@/services/TrainingService";
+
+export default {
+  data() {
+    return {
+      questionsReview: []
+    };
+  },
+  mounted() {
+    this.questionsReview = TrainingService.reviewQuiz(this);
+    this.questionsReview.forEach(question => {
+      if (question.imageSrc) {
+        question.imageStyle = {
+          backgroundImage: `url(${question.imageSrc})`,
+          width: "300px",
+          height: "300px",
+          display: "flex",
+          backgroundSize: "100%",
+          backgroundRepeat: "no-repeat"
+        };
+      } else {
+        question.imageStyle = {};
+      }
+    });
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.question-image {
+  background-position: center;
+}
+
+.question {
+  margin: 50px 0px;
+  text-align: left;
+}
+
+.question-number {
+  font-weight: 600;
+  width: 400px;
+  align-self: center;
+  text-align: left;
+}
+
+.possible-answers {
+  margin: 20px 50px;
+}
+
+.review {
+  width: 600px;
+  margin: 4em auto 0;
+}
+
+.review .question {
+  border-bottom: 0.5px solid #cccccf;
+  padding: 20px;
+  margin: 0px;
+}
+
+@media screen and (max-width: 1000px) {
+  .question-number {
+    width: 100%;
+  }
+
+  .review {
+    width: 100%;
+  }
+}
+
+@media screen and (max-width: 700px) {
+  // override inline styling on image
+  .question-image {
+    width: 100% !important;
+  }
+}
+</style>
