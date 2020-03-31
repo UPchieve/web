4bbed1e2b1dbd6dbaaf9f68efbe5560b346a896f
Trey Stevens <trey.stevens@upchieve.org>
2020-03-31 11:39:54 -0400

    hide "Back to Dashboard" header once component is destroyed

--- src/views/SessionView/index.vue
+++ src/views/SessionView/index.vue
@@ -77,6 +77,9 @@ export default {
 
     window.addEventListener("resize", this.handleResize);
   },
+  beforeDestroy() {
+    window.removeEventListener("resize", this.handleResize);
+  },
   /*
    * @notes
    * [1] Refactoring candidate: it'd be awesome if Dashboard could pass
