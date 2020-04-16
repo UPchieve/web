3ee878c0233bdc2a8fa0074fb932fe8aa2e0ac8a
Trey Stevens <trey.stevens@upchieve.org>
2020-04-16 19:44:33 -0400

    update loading message to use set interval for mobile app ios

--- src/components/LoadingMessage.vue
+++ src/components/LoadingMessage.vue
@@ -1,14 +1,49 @@
 <template>
   <div class="loading-container">
     <span>{{ message }}</span>
-    <span class="loading-ellipsis" />
+    <span v-if="!isMobileAppIOS" class="loading-ellipsis" />
+    <span class="interval-ellipsis">{{ animatedEllipsis }}</span>
   </div>
 </template>
 
 <script>
+import { mapState } from "vuex";
+import getOperatingSystem from "@/utils/get-operating-system";
+
 export default {
   props: {
     message: { type: String, required: true }
+  },
+  data() {
+    return {
+      isMobileAppIOS: false,
+      intervalTimeoutId: null,
+      animatedEllipsis: ""
+    };
+  },
+  computed: {
+    ...mapState({
+      isMobileApp: state => state.app.isMobileApp
+    })
+  },
+  created() {
+    // @todo
+    // @note: animation for LoadingMessage when in the NotificationModal
+    //        is being inconsistent with mobile app ios devices
+    //        even with this workaround
+    if (getOperatingSystem() === "iOS" && this.isMobileApp) {
+      this.isMobileAppIOS = true;
+      this.intervalTimeoutId = setInterval(() => {
+        if (this.animatedEllipsis.length === 3) {
+          this.animatedEllipsis = "";
+        } else {
+          this.animatedEllipsis += ".";
+        }
+      }, 500);
+    }
+  },
+  beforeDestroy() {
+    clearInterval(this.intervalTimeoutId);
   }
 };
 </script>
@@ -31,6 +66,11 @@ export default {
     -webkit-animation: 1.2s ellip infinite;
     animation: 1.2s ellip infinite;
   }
+}
+
+.interval-ellipsis {
+  text-align: left;
+  position: absolute;
 }
 
 @keyframes ellip {
