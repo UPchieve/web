0208f245d3ccb940f5e900cbb3eb6e4bbbe2bf8c
Trey Stevens <trey.stevens@upchieve.org>
2020-04-22 14:24:15 -0400

    added banner template and store

---
+++ src/components/App/AppBanner/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <banner-template :onCancel="onCancel">
+    <component v-if="bannerComponent" v-bind:is="bannerComponent" />
+  </banner-template>
+</template>
+
+<script>
+import { mapState } from "vuex";
+import BannerTemplate from "./BannerTemplate";
+
+export default {
+  components: {
+    BannerTemplate
+  },
+  computed: {
+    ...mapState({
+      bannerComponent: state => state.app.banner.component
+    })
+  },
+  methods: {
+    onCancel() {
+      this.$store.dispatch("app/banner/hide");
+    }
+  }
+};
+</script>
