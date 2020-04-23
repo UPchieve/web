ced771ef95e798bac9f5dc108f4fe22ff9d383a3
Trey <35900194+treykris@users.noreply.github.com>
2020-04-23 17:47:09 -0400

    Show app banner (#390)
    * added banner template and store
    
    * added app store links
    
    * app store logo asset
    
    * added mobile app notice banner
    
    * disable scroll when banner mounts
    
    * show mobile app notice banner when user is present
    
    * removed unused prop
    
    * workaround to watch mobilemode to close banner
    
    * remove unused state  "data"
    
    Co-authored-by: Trey Stevens <trey.stevens@upchieve.org>

---
+++ src/components/App/AppBanner/MobileAppNoticeBanner.vue
@@ -0,0 +1,109 @@
+<template>
+  <div>
+    <p class="banner-description">
+      Visit the {{ mobileAppStore }} to download the UPchieve App!
+    </p>
+    <div class="banner-container">
+      <div class="mobile-app-info">
+        <div class="logo-background">
+          <app-store-logo class="mobile-app-store-logo" />
+        </div>
+        <span class="mobile-app-store-title">
+          UPchieve App
+        </span>
+      </div>
+      <a :href="mobileAppStoreLink" class="mobile-app-store-link">Visit</a>
+    </div>
+  </div>
+</template>
+
+<script>
+import LogoImageUrl from "@/assets/header_logo.png";
+import getOperatingSystem from "@/utils/get-operating-system";
+import AppStoreLogo from "@/assets/app-store-logo.svg";
+import { GOOGLE_PLAY_STORE_LINK, APP_STORE_LINK } from "@/consts";
+
+export default {
+  name: "mobile-app-notice-banner",
+  components: { AppStoreLogo },
+  data() {
+    return { logoUrl: LogoImageUrl };
+  },
+  mounted() {
+    const body = document.querySelector("body");
+    body.classList.add("disable-scroll");
+  },
+  beforeDestroy() {
+    const body = document.querySelector("body");
+    body.classList.remove("disable-scroll");
+  },
+  computed: {
+    mobileAppStoreLink() {
+      if (this.mobileAppStore === "Google Play Store") {
+        return GOOGLE_PLAY_STORE_LINK;
+      }
+      if (this.mobileAppStore === "App Store") {
+        return APP_STORE_LINK;
+      }
+      return "";
+    },
+    mobileAppStore() {
+      const device = getOperatingSystem();
+
+      if (device === "Android") {
+        return "Google Play Store";
+      }
+      if (device === "iOS") {
+        return "App Store";
+      }
+      return "";
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+p {
+  padding: 0;
+  margin: 0;
+}
+
+.banner-description {
+  text-align: left;
+}
+
+.banner-container {
+  @include flex-container(row, space-between, center);
+  margin: 1em 0;
+}
+
+.mobile-app-info {
+  display: flex;
+  align-items: center;
+}
+
+.mobile-app-store-title {
+  font-weight: 500;
+  padding: 0 0.6em;
+}
+
+.mobile-app-store-link {
+  color: $c-success-green;
+  border: 1px solid $c-success-green;
+  border-radius: 5px;
+  padding: 0.4em 0.8em;
+}
+
+.logo-background {
+  background-color: $c-success-green;
+  display: flex;
+  border-radius: 8px;
+  padding: 1em;
+}
+
+.mobile-app-banner-upchieve-logo {
+  & g {
+    fill: white;
+  }
+}
+</style>
--- src/consts.js
+++ src/consts.js
@@ -1,2 +1,8 @@
 /** Maximum width considered to still be in "mobile mode." */
 export const MAX_MOBILE_MODE_WIDTH = 768 - 1; // 768 = "medium" scss breakpoint
+
+export const APP_STORE_LINK =
+  "https://apps.apple.com/us/app/upchieve/id1506076042";
+
+export const GOOGLE_PLAY_STORE_LINK =
+  "https://play.google.com/store/apps/details?id=org.upchieve.mobile";
