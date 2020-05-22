d3a94077ca998dd680fafddb8fcde0d76161a9b9
Trey Stevens <trey.stevens@upchieve.org>
2020-05-22 02:49:05 -0400

    added chemistry and physics to topics

--- src/utils/topics.js
+++ src/utils/topics.js
@@ -12,7 +12,9 @@ export const topics = {
   },
   science: {
     subtopics: {
-      biology: { displayName: "Biology" }
+      biology: { displayName: "Biology" },
+      chemistry: { displayName: "Chemistry" },
+      physics: { displayName: "Physics" }
     },
     displayName: "Science Tutoring"
   },
