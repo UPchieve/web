fc831757cfb0cb5d6f1427b7ee501c6559b9a637
Trey Stevens <trey.stevens@upchieve.org>
2020-06-02 23:15:42 -0400

    volunteer verification modals

---
+++ src/views/DashboardView/VolunteerDashboard/PhotoUploadModal.vue
@@ -0,0 +1,176 @@
+<template>
+  <div>
+    <header>
+      <h1 class="title">Proof of identity</h1>
+      <cross-icon class="cross-icon" @click="closeModal" />
+    </header>
+    <p class="subtitle">
+      Upload a picture of your photo ID so we can verify that it's you. This may
+      be a driver's license, passport, or student ID.
+    </p>
+
+    <div v-if="photo" class="photo-id-container">
+      <img :src="photo" class="photo-id-img" />
+      <div class="trash-icon-container" @click="removePhoto">
+        <trash-icon class="trash-icon" />
+      </div>
+    </div>
+    <label v-else class="photo-id-label">
+      <input type="file" class="photo-id-input" @change="submitPhoto" />
+      <button class="upload-photo-btn">Upload Photo</button>
+    </label>
+
+    <div v-if="!mobileMode" class="seperator" />
+
+    <large-button @click="submitPhoto" class="save-btn" :disabled="!photo">
+      Save
+    </large-button>
+  </div>
+</template>
+
+<script>
+import { mapState, mapGetters } from "vuex";
+import LargeButton from "@/components/LargeButton";
+import TrashIcon from "@/assets/trash.svg";
+import CrossIcon from "@/assets/cross.svg";
+
+export default {
+  name: "volunteer-dashboard",
+  components: { LargeButton, TrashIcon, CrossIcon },
+  data() {
+    return {
+      photo: ""
+    };
+  },
+  mounted() {
+    this.photo = this.user.photo;
+  },
+  computed: {
+    ...mapState({
+      user: state => state.user.user
+    }),
+    ...mapGetters({ mobileMode: "app/mobileMode" })
+  },
+  methods: {
+    // @todo: server side - add photo url to user
+    submitPhoto(event) {
+      const { files } = event.target;
+      const file = files[0];
+      this.photo = URL.createObjectURL(file);
+    },
+    removePhoto() {
+      event.stopPropagation();
+      this.photo = "";
+    },
+    closeModal() {
+      this.$store.dispatch("app/modal/hide");
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+h1,
+h2,
+p {
+  margin: 0;
+  padding: 0;
+}
+
+header {
+  @include flex-container(row, space-between, center);
+  margin-bottom: 2em;
+}
+
+.title {
+  @include font-category("display-small");
+  @include child-spacing(bottom, 18px);
+  text-align: left;
+}
+
+.subtitle {
+  @include font-category("body");
+  text-align: left;
+  color: $c-secondary-grey;
+}
+
+.save-btn {
+  background-color: $c-success-green;
+  border-color: transparent;
+  color: white;
+  margin-left: auto;
+  margin-top: 20px;
+  border: none;
+}
+
+.photo-id-container {
+  position: relative;
+  margin: 2em 0;
+}
+
+.photo-id-img {
+  width: 100%;
+  border-radius: 5px;
+}
+
+.photo-id-label {
+  @include flex-container(row, center, center);
+  border-radius: 8px;
+  margin: 2em 0;
+  border: 3px dashed $c-border-grey;
+  cursor: pointer;
+
+  &:hover {
+    background-color: rgba($color: $c-border-grey, $alpha: 0.2);
+
+    & button {
+      background-color: rgba($color: $c-information-blue, $alpha: 0.7);
+    }
+  }
+}
+
+.photo-id-input {
+  display: none;
+  width: 0;
+}
+
+.upload-photo-btn {
+  width: 160px;
+  border: 1px solid $c-border-grey;
+  border-radius: 8px;
+  color: white;
+  background-color: $c-information-blue;
+  padding: 1.4em 1em;
+  pointer-events: none;
+  margin: 4em 0;
+}
+
+.trash-icon-container {
+  @include flex-container(row, center, center);
+  position: absolute;
+  top: 15px;
+  right: 15px;
+  width: 40px;
+  height: 40px;
+  padding: 0.5em;
+  border-radius: 40px;
+  background-color: rgba($color: $c-soft-black, $alpha: 0.8);
+  cursor: pointer;
+}
+
+.trash-icon {
+  fill: white;
+}
+
+.cross-icon {
+  width: 20px;
+  cursor: pointer;
+}
+
+.seperator {
+  border: 1px solid $c-border-grey;
+  width: 100%;
+  height: 1px;
+  margin-top: 2em;
+}
+</style>
---
+++ src/views/DashboardView/VolunteerDashboard/ReferencesModal.vue
@@ -0,0 +1,357 @@
+<template>
+  <div class="reference-modal">
+    <header>
+      <h1 class="title">Reference check</h1>
+      <cross-icon class="cross-icon" @click="closeModal" />
+    </header>
+    <div v-if="step === 0">
+      <p class="subtitle">
+        Step 1: Add your LinkedIn (optional)
+      </p>
+      <p>
+        Adding your LinkedIn allows us to quickly verify your credentials and
+        require 1 less reference from you!
+      </p>
+      <input
+        type="text"
+        v-model="linkedIn"
+        placeholder="linkedin.com/in/example"
+        class="linkedin-input"
+      />
+    </div>
+    <div v-else-if="step === 1">
+      <p class="subtitle">
+        Step 2: Provide {{ totalReferencesNeeded }} reference{{
+          totalReferencesNeeded === 1 ? "" : "s"
+        }}
+      </p>
+      <p>
+        Please provide the name and email for your references. We will contact
+        them via email requesting them to fill out a reference form
+      </p>
+
+      <div class="references-container">
+        <div
+          v-for="(reference, index) in references"
+          :key="reference.email"
+          class="reference"
+        >
+          <div class="reference-header">
+            <h4>Reference {{ index + 1 }}</h4>
+            <trash-icon
+              class="trash-icon"
+              v-if="reference.status !== 'VERIFIED'"
+              @click="removeReference(index)"
+            />
+          </div>
+          <p>{{ reference.name }}</p>
+          <p>{{ reference.email }}</p>
+          <div class="reference-status">
+            <span
+              class="reference-status-indicator"
+              :class="{
+                verified: reference.status === 'VERIFIED',
+                pending: reference.status === 'WAITING ON RESPONSE',
+                rejected: reference.status === 'REJECTED'
+              }"
+            ></span>
+            <span>{{ reference.status }}</span>
+          </div>
+        </div>
+      </div>
+
+      <div v-if="isAddReferenceMode" class="add-reference-container">
+        <div class="reference">
+          <header class="reference-header">
+            <h4>Reference {{ references.length + 1 }}</h4>
+            <trash-icon class="trash-icon" @click="toggleAddReferenceMode" />
+          </header>
+          <label for="reference-name">Name</label>
+          <input type="text" id="reference-name" v-model="newReferenceName" />
+          <label for="reference-email">Email</label>
+          <input
+            type="email"
+            id="reference-email"
+            v-model="newReferenceEmail"
+          />
+          <large-button
+            class="btn save-btn"
+            @click.native="addReference"
+            :disabled="!(newReferenceName && newReferenceEmail)"
+            >Save</large-button
+          >
+        </div>
+      </div>
+
+      <button
+        class="add-reference-btn"
+        @click="toggleAddReferenceMode"
+        v-if="!isAddReferenceMode && references.length < totalReferencesNeeded"
+      >
+        Add reference
+      </button>
+    </div>
+
+    <div v-if="!mobileMode" class="seperator" />
+
+    <div class="buttons-container">
+      <large-button class="btn" @click.native="previousStep" v-if="step === 1"
+        >Back</large-button
+      >
+
+      <large-button
+        class="btn skip-btn"
+        @click.native="nextStep"
+        v-if="step === 0 && !linkedIn"
+        >Skip</large-button
+      >
+      <large-button
+        class="btn skip-btn"
+        @click.native="addLinkedIn"
+        v-if="step === 0 && linkedIn"
+        >Next</large-button
+      >
+
+      <large-button
+        class="btn done-btn"
+        v-if="step === 1"
+        @click.native="closeModal"
+        >Done</large-button
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState, mapGetters } from "vuex";
+import LargeButton from "@/components/LargeButton";
+import TrashIcon from "@/assets/trash.svg";
+import CrossIcon from "@/assets/cross.svg";
+
+export default {
+  name: "reference-modal",
+  components: { LargeButton, TrashIcon, CrossIcon },
+  data() {
+    return {
+      linkedIn: "",
+      references: [],
+      newReferenceName: "",
+      newReferenceEmail: "",
+      step: 0,
+      isAddReferenceMode: false
+    };
+  },
+  mounted() {
+    this.references = this.user.references || [
+      {
+        name: "Jane Doe",
+        email: "janedoe@gmail.com",
+        status: "WAITING ON RESPONSE"
+      }
+    ];
+  },
+  computed: {
+    ...mapState({
+      user: state => state.user.user
+    }),
+    ...mapGetters({ mobileMode: "app/mobileMode" }),
+    totalReferencesNeeded() {
+      return this.linkedIn ? 1 : 2;
+    },
+    stepOneButtonText() {
+      return this.linkedIn ? "Next" : "Skip";
+    }
+  },
+  methods: {
+    nextStep() {
+      this.step += 1;
+    },
+    previousStep() {
+      this.step -= 1;
+    },
+    toggleAddReferenceMode() {
+      this.newReferenceName = "";
+      this.newReferenceEmail = "";
+      this.isAddReferenceMode = !this.isAddReferenceMode;
+    },
+    // @todo: server side - save the new reference
+    addReference() {
+      const newReference = {
+        name: this.newReferenceName,
+        email: this.newReferenceEmail,
+        status: "WAITING ON RESPONSE"
+      };
+      this.references.push(newReference);
+      this.toggleAddReferenceMode();
+    },
+    // @todo: server side - remove the reference
+    removeReference(position) {
+      this.references = this.references.filter(
+        (_, index) => position !== index
+      );
+    },
+    // @todo: server side - save the linkedIn
+    addLinkedIn() {
+      this.nextStep();
+    },
+    closeModal() {
+      this.$store.dispatch("app/modal/hide");
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+$border-radius: 5px;
+
+h1,
+h2,
+p {
+  margin: 0;
+  padding: 0;
+}
+
+header {
+  @include flex-container(row, space-between, center);
+  margin-bottom: 2em;
+}
+
+input {
+  display: block;
+  border-radius: $border-radius;
+  border: 1px solid $c-border-grey;
+  width: 100%;
+  padding: 0.4em 0 0.4em 0.6em;
+
+  &::placeholder {
+    color: $c-secondary-grey;
+  }
+}
+
+.linkedin-input {
+  margin: 1.4em 0;
+}
+
+label {
+  @include font-category("body");
+  margin: 0.6em 0;
+}
+
+.reference-modal {
+  text-align: left;
+}
+
+.title {
+  @include font-category("display-small");
+  @include child-spacing(bottom, 18px);
+}
+
+.subtitle {
+  @include font-category("body");
+  font-weight: 500;
+}
+
+.btn {
+  margin-top: 20px;
+}
+
+.skip-btn,
+.save-btn {
+  margin-left: auto;
+}
+
+.done-btn {
+  background-color: $c-success-green;
+  border-color: transparent;
+  color: white;
+  border: none;
+}
+
+.references-container,
+.add-reference-container {
+  margin-top: 2em;
+}
+
+.references-list {
+  list-style-type: none;
+  padding-left: 0;
+}
+
+.buttons-container {
+  @include flex-container(row, space-between, center);
+}
+
+.trash-icon {
+  fill: $c-soft-black;
+  width: 25px;
+}
+
+.reference {
+  @include font-category("body");
+  border-radius: $border-radius;
+  padding: 1em;
+  box-shadow: 0px 0px 5px 0px rgba(166, 166, 166, 0.77);
+  margin-bottom: 2em;
+
+  &-name {
+    font-weight: 500;
+  }
+}
+
+.reference-header {
+  @include flex-container(row, space-between, center);
+  color: $c-secondary-grey;
+  @include font-category("body");
+}
+
+.reference-status {
+  @include flex-container(row, flex-start, center);
+  margin-top: 0.8em;
+  text-transform: uppercase;
+  color: $c-secondary-grey;
+
+  &-indicator {
+    display: inline-block;
+    width: 12px;
+    height: 12px;
+    border-radius: 50%;
+    margin-right: 0.6em;
+  }
+}
+
+.verified {
+  background-color: $c-success-green;
+}
+
+.pending {
+  background-color: $c-warning-orange;
+}
+
+.rejected {
+  background-color: $c-error-red;
+}
+
+.add-reference-btn {
+  @include font-category("body");
+  width: 160px;
+  padding: 0.6em 0;
+  background-color: $c-information-blue;
+  color: white;
+  border-radius: 30px;
+  border: none;
+  display: block;
+  margin: 1.4em auto;
+}
+
+.seperator {
+  border: 1px solid $c-border-grey;
+  width: 100%;
+  height: 1px;
+  margin-top: 2em;
+}
+
+.cross-icon {
+  width: 20px;
+  cursor: pointer;
+}
+</style>
