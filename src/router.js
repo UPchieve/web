1b6bbfcd1286976ff5494d49bee96c7b45f061b1
Jack Keating <jack.keating@upchieve.org>
2020-07-10 15:19:32 -0500

    define vue router's scroll behavior

--- src/router.js
+++ src/router.js
@@ -287,7 +287,10 @@ const routes = [
 const router = new VueRouter({
   routes,
   linkActiveClass: "active",
-  mode: "history"
+  mode: "history",
+  scrollBehavior() {
+    return { x: 0, y: 0 };
+  }
 });
 
 export default router;
