6d1db4d9e91b65414d69b728b67bdefde45aef3c
Trey Stevens <trey.stevens@upchieve.org>
2020-07-14 13:34:21 -0400

    network service for reports

--- src/services/NetworkService.js
+++ src/services/NetworkService.js
@@ -269,6 +269,46 @@ export default {
       .post(`${ELIGIBILITY_API_ROOT}/school/approval`, data)
       .then(this._successHandler, this._errorHandler);
   },
+  adminGetSessionReport({
+    joinedBefore,
+    joinedAfter,
+    sessionRangeFrom,
+    sessionRangeTo,
+    highSchoolId,
+    studentPartnerOrg
+  }) {
+    const queryParams = new URLSearchParams({
+      joinedBefore,
+      joinedAfter,
+      sessionRangeFrom,
+      sessionRangeTo,
+      highSchoolId,
+      studentPartnerOrg
+    }).toString();
+    return Vue.http
+      .get(`${API_ROOT}/reports/session-report?${queryParams}`)
+      .then(this._successHandler, this._errorHandler);
+  },
+  adminGetUsageReport({
+    joinedBefore,
+    joinedAfter,
+    sessionRangeFrom,
+    sessionRangeTo,
+    highSchoolId,
+    studentPartnerOrg
+  }) {
+    const queryParams = new URLSearchParams({
+      joinedBefore,
+      joinedAfter,
+      sessionRangeFrom,
+      sessionRangeTo,
+      highSchoolId,
+      studentPartnerOrg
+    }).toString();
+    return Vue.http
+      .get(`${API_ROOT}/reports/usage-report?${queryParams}`)
+      .then(this._successHandler, this._errorHandler);
+  },
   getQuestions(context, data) {
     return context.$http
       .post(`${API_ROOT}/training/questions`, data)
