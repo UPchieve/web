f4a341eb49984d01f302bc158f98da1d70f0be0d
Trey Stevens <trey.stevens@upchieve.org>
2020-07-14 13:31:18 -0400

    school list component

---
+++ src/components/SchoolList.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <autocomplete
+      :search="autocompleteSchool"
+      :get-result-value="getSchoolDisplayName"
+      base-class="uc-autocomplete"
+      auto-select
+      :placeholder="placeholder"
+      :aria-label="placeholder"
+      @submit="handleSelectHighSchool"
+      @input="setHighSchool"
+    ></autocomplete>
+  </div>
+</template>
+
+<script>
+import Autocomplete from "@trevoreyre/autocomplete-vue";
+import NetworkService from "@/services/NetworkService";
+
+export default {
+  props: {
+    setHighSchool: {
+      type: Function,
+      required: true
+    },
+    placeholder: {
+      type: String
+    }
+  },
+  components: {
+    Autocomplete
+  },
+
+  methods: {
+    async autocompleteSchool(input) {
+      if (input.length < 3) return [];
+
+      const response = await NetworkService.searchSchool(this, {
+        query: input
+      });
+      const {
+        body: { results: schools }
+      } = response;
+      return schools;
+    },
+    getSchoolDisplayName(school) {
+      return `${school.name} (${school.city}, ${school.state})`;
+    },
+    handleSelectHighSchool(school) {
+      this.setHighSchool(school);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
