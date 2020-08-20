9378dc3d1e2747b84afcce0a1e73357436e901ac
Jack <keatjm@gmail.com>
2020-08-20 17:25:25 -0500

    slideshow material component (done)

--- src/views/TrainingCourseView/Material/Slideshow.vue
+++ src/views/TrainingCourseView/Material/Slideshow.vue
@@ -1,1 +1,32 @@
-// placeholder
+<template>
+  <div class="slideshow">
+    <iframe
+      :src="
+        `https://docs.google.com/presentation/d/e/${resourceId}/embed?start=true&loop=false&delayms=30000`
+      "
+      frameborder="0"
+      width="auto"
+      height="569"
+      allowfullscreen="true"
+      mozallowfullscreen="true"
+      webkitallowfullscreen="true"
+    ></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    resourceId: String
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.slideshow {
+  iframe {
+    width: 100%;
+    height: 500px;
+  }
+}
+</style>
