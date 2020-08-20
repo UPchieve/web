2fe6e020baf7b630bcae82c5799a06e3188422cc
Jack <keatjm@gmail.com>
2020-08-20 17:26:06 -0500

    video + document components (WIP)

--- src/views/TrainingCourseView/Material/Document.vue
+++ src/views/TrainingCourseView/Material/Document.vue
@@ -1,1 +1,27 @@
-// placeholder
+<template>
+  <div class="document">
+    <iframe
+      :src="
+        `https://docs.google.com/document/d/e/${resourceId}/pub?embedded=true`
+      "
+    ></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    resourceId: String
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.document {
+  iframe {
+    width: 100%;
+    height: 400px;
+    border: solid $c-border-grey 1px;
+  }
+}
+</style>
--- src/views/TrainingCourseView/Material/Video.vue
+++ src/views/TrainingCourseView/Material/Video.vue
@@ -1,1 +1,19 @@
-// placeholder
+<template>
+  <div class="video">
+    <h4>Resource ID: {{ resourceId }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    resourceId: String
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.video {
+  color: black;
+}
+</style>
