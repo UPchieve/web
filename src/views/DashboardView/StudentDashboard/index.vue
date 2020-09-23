858fcdd2d8c1a96d7f4f5c0ec37d6d4a2e0d3bb7
Trey <35900194+treykris@users.noreply.github.com>
2020-09-23 17:45:26 -0400

    Low coach banner (#536)
    * toggle account status on profile
    
    * show dashboard message for low coach hours
    
    * moment -> moment-timezone
    
    Co-authored-by: Jack <keatjm@gmail.com>
    Co-authored-by: Trey Stevens <trey.stevens@upchieve.org>

--- src/views/DashboardView/StudentDashboard/index.vue
+++ src/views/DashboardView/StudentDashboard/index.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="student-dashboard">
     <dashboard-banner />
+    <div
+      v-if="noticeMessage"
+      class="dashboard-notice"
+      :class="isLowCoachHour && 'dashboard-notice--warn'"
+    >
+      {{ noticeMessage }}
+    </div>
     <subject-selection />
   </div>
 </template>
@@ -9,6 +16,7 @@
 import { mapGetters, mapState } from "vuex";
 import DashboardBanner from "../DashboardBanner";
 import SubjectSelection from "./SubjectSelection";
+import moment from "moment-timezone";
 
 const headerData = {
   component: "RejoinSessionHeader",
@@ -31,12 +39,37 @@ export default {
         }
       });
     }
+
+    this.currentHour = moment()
+      .tz("America/New_York")
+      .hour();
+  },
+  data() {
+    return {
+      currentHour: 0
+    };
   },
   computed: {
     ...mapState({
       isFirstDashboardVisit: state => state.user.isFirstDashboardVisit
     }),
-    ...mapGetters({ isSessionAlive: "user/isSessionAlive" })
+    ...mapGetters({ isSessionAlive: "user/isSessionAlive" }),
+    isLowCoachHour() {
+      return this.currentHour < 12;
+    },
+    noticeMessage() {
+      if (this.currentHour >= 12 && this.currentHour <= 23)
+        return "Heads up: this is a great time to make a request! We have plenty of coaches available between 12pm - 12 am ET.";
+      if (this.currentHour >= 3 && this.currentHour <= 9)
+        return "Heads up: we have very few coaches available right now. Try making requests between 12pm-12am ET when possible";
+      if (
+        (this.currentHour >= 0 && this.currentHour < 3) ||
+        (this.currentHour >= 9 && this.currentHour < 12)
+      )
+        return "Heads up: we have less coaches available than normal right now. Try making requests between 12pm-12am ET when possible!";
+
+      return "";
+    }
   },
   watch: {
     isSessionAlive(isAlive) {
@@ -60,6 +93,20 @@ export default {
     display: inline-flex;
     min-width: 100%;
     padding: 40px;
+  }
+}
+
+.dashboard-notice {
+  padding: 15px;
+  background-color: $c-success-green;
+  border-radius: 8px;
+  margin: 20px 0 -20px;
+  font-weight: 500;
+  font-size: 16px;
+  color: #fff;
+
+  &--warn {
+    background-color: $c-warning-orange;
   }
 }
 </style>
