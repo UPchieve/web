9ebd13684b6c309110ee6d8d71766c9226074775
Jack <keatjm@gmail.com>
2020-09-28 17:00:47 -0500

    don't let radio buttons shrink

--- src/views/DashboardView/StudentDashboard/SubjectSelection/PresessionSurvey.vue
+++ src/views/DashboardView/StudentDashboard/SubjectSelection/PresessionSurvey.vue
@@ -202,6 +202,10 @@ export default {
 
     input[type="radio"] {
       margin: 0 8px 0 0;
+      padding: 0;
+      height: 16px;
+      width: 16px;
+      flex-shrink: 0;
     }
 
     input[type="text"] {
