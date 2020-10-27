c3750b50be2adfe5cb681f45425f5609b1d5490d
Trey Stevens <trey.stevens@upchieve.org>
2020-10-27 00:12:11 -0400

    modals to show when exceeded a certain session waiting time

---
+++ src/views/SessionView/TroubleMatchingModal.vue
@@ -0,0 +1,76 @@
+<template>
+  <modal :closeModal="() => false">
+    <div class="trouble-matching-modal">
+      <header>
+        <h1 class="trouble-matching-modal__title">
+          We’re having trouble matching you with a coach 😔
+        </h1>
+      </header>
+
+      <h2 class="trouble-matching-modal__subtitle">
+        We recommend ending your session and trying again later.
+      </h2>
+      <separator />
+      <div class="trouble-matching-modal__footer">
+        <div class="trouble-matching-modal__buttons">
+          <large-button
+            @click.native="closeModal"
+            class="trouble-matching-modal__buttons--cancel"
+            >Keep Waiting</large-button
+          >
+          <large-button primary @click.native="endSession"
+            >End Session</large-button
+          >
+        </div>
+      </div>
+    </div>
+  </modal>
+</template>
+
+<script>
+import Modal from "@/components/Modal";
+import Separator from "@/components/Separator";
+import LargeButton from "@/components/LargeButton";
+
+export default {
+  name: "TroubleMatchingModal",
+  components: { LargeButton, Modal, Separator },
+  props: {
+    endSession: { type: Function, required: true },
+    closeModal: { type: Function, required: true }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.trouble-matching-modal {
+  @include flex-container(column);
+
+  &__title {
+    @include font-category("display-small");
+  }
+
+  &__subtitle {
+    @include font-category("body");
+    margin: 0 0 35px;
+    color: $c-secondary-grey;
+    font-size: 15px;
+  }
+
+  &__buttons {
+    margin-top: 16px;
+    @include flex-container(row, flex-end);
+    @include child-spacing(left, 16px);
+
+    &--cancel {
+      background-color: $c-disabled-grey;
+      color: #fff;
+
+      &:hover {
+        border: 1px solid transparent;
+        background-color: darken($c-disabled-grey, 5%);
+      }
+    }
+  }
+}
+</style>
---
+++ src/views/SessionView/UnmatchedModal.vue
@@ -0,0 +1,63 @@
+<template>
+  <modal :closeModal="() => false">
+    <div class="unmatched-modal">
+      <header>
+        <h1 class="unmatched-modal__title">
+          Sorry! We couldn’t match your request this time.
+        </h1>
+      </header>
+
+      <h2 class="unmatched-modal__subtitle">
+        Please try again between 12 pm -12 am ET. More coaches will be around to
+        help you then 😊
+      </h2>
+
+      <separator />
+
+      <footer class="unmatched-modal__footer">
+        <div class="unmatched-modal__buttons">
+          <large-button primary @click.native="endSession"
+            >Back to dashboard</large-button
+          >
+        </div>
+      </footer>
+    </div>
+  </modal>
+</template>
+
+<script>
+import Modal from "@/components/Modal";
+import Separator from "@/components/Separator";
+import LargeButton from "@/components/LargeButton";
+
+export default {
+  name: "UnmatchedModal",
+  components: { LargeButton, Modal, Separator },
+  props: {
+    endSession: { type: Function, required: true }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.unmatched-modal {
+  @include flex-container(column);
+
+  &__title {
+    @include font-category("display-small");
+  }
+
+  &__subtitle {
+    @include font-category("body");
+    margin: 0 0 35px;
+    color: $c-secondary-grey;
+    font-size: 15px;
+  }
+
+  &__buttons {
+    margin-top: 16px;
+    @include flex-container(row, flex-end);
+    @include child-spacing(left, 16px);
+  }
+}
+</style>
