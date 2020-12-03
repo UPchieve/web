8be72a9bb52846ad72fb9e74c77df327b5d1d696
Dave Sudia <dsudia@gospotcheck.com>
2020-12-03 11:17:10 -0700

    adjust config code so tests also work

--- src/config.js
+++ src/config.js
@@ -1,26 +1,39 @@
 const configElement = document.getElementById("config");
-const passedConfig = JSON.parse(configElement.innerHTML);
 
 const config = {};
 
-// the local dev server won't interpolate in the values so we need
-// to provide defaults for that case
-// if there's no interpolation the object will include straight string values
-// of the mustache templates, so we can look for that
-passedConfig.mainWebsiteUrl.includes("{{")
+let passedConfig;
+if (configElement !== null) {
+  passedConfig = JSON.parse(configElement.innerHTML);
+  // the local dev server won't interpolate in the values so we need
+  // to provide defaults for that case
+  // if there's no interpolation the object will include straight string values
+  // of the mustache templates, so we can look for that
+  passedConfig.mainWebsiteUrl.includes("{{")
   ? (config.mainWebsiteUrl = "http://www.upchieve.org")
-  : (config.serverRoot = passedConfig.mainWebsiteUrl);
-passedConfig.serverRoot.includes("{{")
+  : (config.mainWebsiteUrl = passedConfig.mainWebsiteUrl);
+  passedConfig.serverRoot.includes("{{")
   ? (config.serverRoot = "http://localhost:3000")
   : (config.serverRoot = passedConfig.serverRoot);
-passedConfig.socketAddress.includes("{{")
+  passedConfig.socketAddress.includes("{{")
   ? (config.websocketAddress = "http://localhost:3001")
   : (config.socketAddress = passedConfig.socketAddress);
-passedConfig.websocketRoot.includes("{{")
+  passedConfig.websocketRoot.includes("{{")
   ? (config.websocketRoot = "ws://localhost:3000")
   : (config.websocketRoot = passedConfig.websocketRoot);
-passedConfig.zwibblerUrl.includes("{{")
+  passedConfig.zwibblerUrl.includes("{{")
   ? (config.zwibblerUrl = "/static/js/zwibbler-demo.js")
   : (config.zwibblerUrl = passedConfig.zwibblerUrl);
+} else {
+  // tests don't include the index.html file
+  // so we have to populate these without it
+  config.mainWebsiteUrl = "http://www.upchieve.org"
+  config.serverRoot = "http://localhost:3000"
+  config.websocketAddress = "http://localhost:3001"
+  config.websocketRoot = "ws://localhost:3000"
+  config.zwibblerUrl = "/static/js/zwibbler-demo.js"
+}
+
+
 
 export default config;
