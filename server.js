9d49ba76b2f5c7c0fadcaaee9a789132c8bb0c03
Dave Sudia <dsudia@gospotcheck.com>
2020-12-02 20:09:00 -0700

    poc server and refactor of config keys

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const path = require("path")
+const fs = require("fs")
+const Mustache = require("mustache")
+
+let template = ""
+
+try {
+  template = fs.readFileSync('dist/index.html', 'utf8')
+} catch (err) {
+  console.error("no index.html template in dist directory")
+  process.exit(1)
+}
+// speeds up rendering on the first request
+Mustache.parse(template)
+
+const config = {
+  zwibblerUrl: process.env.VUE_APP_ZWIBBLER_URL,
+  websocketRoot: process.env.VUE_APP_WEBSOCKET_ROOT,
+  serverRoot: process.env.VUE_APP_SERVER_ROOT,
+  socketAddress: process.env.VUE_APP_SOCKET_ADDRESS,
+  mainWebsiteUrl: process.env.VUE_APP_MAIN_WEBSITE_URL
+}
+
+const app = express()
+
+app.get("/", function(_, res) {
+  output = Mustache.render(template, config)
+  res.send(output).status(200)
+})
+
+app.listen(3000, () => {
+  console.log("listening on 3000")
+})
--- src/config.js
+++ src/config.js
@@ -5,10 +5,12 @@ const config = {}
 
 // the local dev server won't interpolate in the values so we need
 // to provide defaults for that case
-passedConfig.VUE_APP_MAIN_WEBSITE_URL.includes("<%=") ? config.mainWebsiteUrl = "http://www.upchieve.org" : config.serverRoot = passedConfig.VUE_APP_MAIN_WEBSITE_URL
-passedConfig.VUE_APP_SERVER_ROOT.includes("<%=") ? config.serverRoot = "http://localhost:3000" : config.serverRoot = passedConfig.VUE_APP_SERVER_ROOT
-passedConfig.VUE_APP_SOCKET_ADDRESS.includes("<%=") ? config.websocketAddress = "http://localhost:3001" : config.socketAddress = passedConfig.VUE_APP_SOCKET_ADDRESS
-passedConfig.VUE_APP_WEBSOCKET_ROOT.includes("<%=") ? config.websocketRoot = "ws://localhost:3000" : config.websocketRoot = passedConfig.VUE_APP_WEBSOCKET_ROOT
-passedConfig.VUE_APP_ZWIBBLER_URL.includes("<%=") ? config.zwibblerUrl = "/static/js/zwibbler-demo.js" : config.zwibblerUrl = passedConfig.ZWIBBLER_URL
+// if there's no interpolation the object will include straight string values
+// of the mustache templates, so we can look for that
+passedConfig.mainWebsiteUrl.includes("{{") ? config.mainWebsiteUrl = "http://www.upchieve.org" : config.serverRoot = passedConfig.mainWebsiteUrl
+passedConfig.serverRoot.includes("{{") ? config.serverRoot = "http://localhost:3000" : config.serverRoot = passedConfig.serverRoot
+passedConfig.socketAddress.includes("{{") ? config.websocketAddress = "http://localhost:3001" : config.socketAddress = passedConfig.socketAddress
+passedConfig.websocketRoot.includes("{{") ? config.websocketRoot = "ws://localhost:3000" : config.websocketRoot = passedConfig.websocketRoot
+passedConfig.zwibblerUrl.includes("{{") ? config.zwibblerUrl = "/static/js/zwibbler-demo.js" : config.zwibblerUrl = passedConfig.zwibblerUrl
 
 export default config
