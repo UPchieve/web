f680ba22a562e92de5d00d19cbe8ed590e3ad4ad
dmnisson <43731303+dmnisson@users.noreply.github.com>
2020-12-16 17:49:35 -0800

    App sidebar keyboard accessibility (#596)
    * trigger navigation on enter key
    
    * don't let tab key navigate to an invisible sidebar
    
    * make hamburger button tab accessible
    
    * add comment
    
    * remove unnecessary conditional expression

--- src/components/App/AppHeader/DefaultHeader/index.vue
+++ src/components/App/AppHeader/DefaultHeader/index.vue
@@ -6,7 +6,7 @@
         <span class="DefaultHeader-user-info-name">{{ name }}</span>
       </div>
 
-      <hamburger-button />
+      <hamburger-button tabindex="0" />
     </template>
 
     <template v-else>
