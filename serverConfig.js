648cf5021821552d55d31e1de90b16b85b025f96
Dave Sudia <dave.sudia@upchieve.org>
2022-09-06 11:13:21 -0600

    update serverConfig

--- serverConfig.js
+++ serverConfig.js
@@ -1,7 +1,13 @@
 module.exports = {
   nodeEnv: process.env.NODE_ENV || 'dev',
-  host: process.env.HOST || 'http://localhost:8080',
+  host: process.env.HOST || 'localhost:8080',
+  additionalAllowedOrigins: '',
   serverPort: process.env.HIGH_LINE_PORT || 8080,
   version: process.env.HIGH_LINE_VERSION || 'development',
   logLevel: process.env.HIGH_LINE_LOG_LEVEL || 'debug',
+  awsS3: {
+    photoIdBucket: process.env.SUBWAY_PHOTO_ID_BUCKET || 'photo-id-bucket',
+    sessionPhotoBucket:
+      process.env.SUBWAY_SESSION_PHOTO_BUCKET || 'session-photo-bucket',
+  },
 }
