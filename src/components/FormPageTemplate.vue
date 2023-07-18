61c5763e68bbbe5f633090ecc0480b590214e20b
Alex Lindsay <alex.lindsay@upchieve.org>
2023-07-18 14:19:34 +0000

    Improve sign up forms on tiny screens.

--- src/components/FormPageTemplate.vue
+++ src/components/FormPageTemplate.vue
@@ -1,44 +1,98 @@
 <template>
-  <div class="uc-column justify-between h-full">
+  <div class="uc-column h-full">
     <div class="FormPageTemplate">
-      <header>
-        <img class="logo" src="~@/assets/p_logo_white.png" alt="UPchieve" />
-      </header>
+      <img class="logo-white" src="@/assets/p_logo_white.png" alt="UPchieve" />
+      <img class="logo-teal" src="@/assets/header_logo.png" alt="UPchieve" />
       <main>
         <slot></slot>
       </main>
     </div>
-    <form-footer />
+    <nav class="FormFooter" aria-label="More information">
+      <div>
+        <router-link to="/contact">Contact Us</router-link>
+      </div>
+      <div>
+        <router-link to="/legal" target="_blank">Legal Policy</router-link>
+      </div>
+      <div>
+        <a href="https://upchieve.org/" target="_blank">Our Website</a>
+      </div>
+    </nav>
   </div>
 </template>
 
-<script>
-import FormFooter from '@/components/FormFooter'
-export default {
-  components: {
-    FormFooter,
-  },
-}
-</script>
-
 <style lang="scss" scoped>
 .FormPageTemplate {
-  @include flex-container(column, center, center);
+  @include flex-container(column, flex-start, center);
+  background-color: white;
+  padding-top: 20px;
+  height: 100%;
 
   main {
     @include flex-container(column, center, center);
+    padding-bottom: 100px;
     width: 100%;
   }
 
-  background: url('~@/assets/onboarding_background.png') no-repeat center;
-  background-size: cover;
+  @include breakpoint-above('tiny') {
+    background: url('~@/assets/onboarding_background.png') no-repeat center;
+    background-color: blue;
+    background-size: cover;
+    justify-content: center;
+    padding-top: 0;
 
-  height: 100%;
-  min-width: 300px;
+    main {
+      padding-bottom: 50px;
+    }
+  }
 }
 
-.logo {
+.logo-white {
   max-width: 155px;
   margin-bottom: 20px;
+  margin-top: 20px;
+  display: none;
+
+  @include breakpoint-above('tiny') {
+    display: block;
+  }
+}
+
+.logo-teal {
+  display: block;
+  max-width: 95px;
+
+  @include breakpoint-above('tiny') {
+    display: none;
+  }
+}
+
+.FormFooter {
+  @include flex-container(column, space-around, center);
+
+  background-color: #f6f6f6;
+  border-top: 1px solid #cccccf;
+
+  font-size: 12px;
+  font-weight: 600;
+
+  min-height: 100px;
+  margin-top: auto;
+  width: 100%;
+
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+
+  a {
+    color: #73737a;
+    text-transform: uppercase;
+  }
+
+  @include breakpoint-above('tiny') {
+    flex-direction: row;
+    min-height: 50px;
+  }
 }
 </style>
