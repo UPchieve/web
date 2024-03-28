0c0fe7a2d87817c812a2dad332d9ceb90f641e29
Alex Lindsay <alex.lindsay@upchieve.org>
2024-03-28 17:36:51 +0000

    Basic proof of concept for dynamically creating sign-up forms.

---
+++ src/services/SignUpService.js
@@ -0,0 +1,125 @@
+import AnalyticsService from '@/services/AnalyticsService'
+import { EVENTS } from '@/consts'
+
+const RoutePath = {
+  account: '/sign-up/student/account',
+  eligibility: '/sign-up/student/eligibility',
+  ineligible: '/sign-up/student/ineligible',
+  international: '/sign-up/student/international',
+}
+/*
+type PageDetail = {
+  backgroundLayout: 'card' | 'panel-right-50p' etc.,
+  submitAction: (fd: FormData) => string[],
+  classes: string,
+  rows: [
+    classes: string,
+    elements: {
+      element: string,
+      classes: string,
+      content: string,
+      submitAction: (fd: FormData) => string[],
+      props: Object
+    }[],
+}
+*/
+export function getPageDetails(to, from) {
+  if (isIneligibleRoute(to, from)) {
+    return getIneligiblePageDetails()
+  }
+
+  return {
+    backgroundLayout: 'panel-right-50p',
+    submitAction: checkEligibility,
+    rows: [
+      getRow(
+        null,
+        getTextElement('h1', 'Check if you are eligible for UPchieve')
+      ),
+      getRow(
+        'uc-row justify-start',
+        getTextElement('p', 'Already have an account?'),
+        {
+          element: 'router-link',
+          classes: 'uc-link ml-1',
+          content: 'Log In',
+          props: {
+            to: '/login',
+          },
+        }
+      ),
+      getRow(null, {
+        element: 'FormEmail',
+        props: { name: 'studentEmail ' },
+      }),
+      getRow(null, { element: 'br' }),
+      getRow(null, getButtonElement(checkEligibility, 'Submit')),
+    ],
+  }
+}
+
+function checkEligibility(formData) {
+  console.info('check eligibility with ', formData)
+  return [RoutePath.ineligible, 'This would be error.']
+}
+
+function ineligibleContinue() {
+  AnalyticsService.captureEvent(EVENTS.STUDENT_CLICKED_STUDENT_ACCESS_PAGE)
+  window.location = 'https://upchieve.org/request-access'
+}
+
+function getRow(classes = '', ...elements) {
+  return {
+    classes,
+    elements,
+  }
+}
+
+function getTextElement(element, content) {
+  return {
+    element,
+    content,
+  }
+}
+
+function getButtonElement(submitAction, content, classes) {
+  return {
+    element: 'button',
+    classes: 'uc-form-button ' + classes,
+    content,
+    submitAction,
+  }
+}
+
+function isIneligibleRoute(to) {
+  return to.path === RoutePath.ineligible
+}
+
+function getIneligiblePageDetails() {
+  return {
+    backgroundLayout: 'full',
+    submitAction: ineligibleContinue,
+    classes: 'text-center screen-narrow',
+    rows: [
+      getRow('uc-row justify-center', {
+        element: 'updog-crying',
+        classes: 'updog-crying',
+      }),
+      getRow(
+        'uc-row justify-center',
+        getTextElement('h1', "Oops... looks like you're not eligible")
+      ),
+      getRow(
+        null,
+        getTextElement(
+          'p',
+          "While we weren't able to verify your eligibility based on the info provided, don't worry: you may still be eligible! We just need your parent/guardian to answer some more questions first!"
+        )
+      ),
+      getRow(
+        null,
+        getButtonElement(ineligibleContinue, 'Continue', 'button-narrow')
+      ),
+    ],
+  }
+}
---
+++ src/views/SignupView/SignUpForms.vue
@@ -0,0 +1,123 @@
+<template>
+  <form-page-template :layout="pageDetails.backgroundLayout">
+    <FormErrors :errors="error ? [error] : []" />
+
+    <form
+      id="form"
+      class="h-full form"
+      :class="pageDetails.classes"
+      @submit.prevent="submitWithValidation.bind(pageDetails.submitAction)"
+    >
+      <div
+        class="uc-row el-gap"
+        :class="row.classes"
+        v-for="(row, i) in pageDetails.rows"
+        :key="`${i}_${$route.path}`"
+      >
+        <component
+          v-for="(el, j) in row.elements"
+          :key="`${i}-${j}_${$route.path}`"
+          :is="el.element"
+          :class="el.classes"
+          :disabled="
+            el.submitAction ? v$.$error || !!v$.$silentErrors.length : false
+          "
+          v-bind="el.props"
+          @click="
+            el.submitAction ? submitWithValidation(el.submitAction) : null
+          "
+        >
+          {{ el.content }}
+        </component>
+      </div>
+    </form>
+  </form-page-template>
+</template>
+
+<script>
+import { useVuelidate } from '@vuelidate/core'
+import UpdogCrying from '@/assets/updog-crying.svg'
+import FormEmail from '@/components/FormEmail.vue'
+import FormErrors from '@/components/FormErrors.vue'
+import FormPageTemplate from '@/components/FormPageTemplate.vue'
+import * as SignUpService from '@/services/SignUpService'
+
+export default {
+  name: 'sign-up-forms',
+  components: {
+    UpdogCrying,
+    FormEmail,
+    FormErrors,
+    FormPageTemplate,
+  },
+
+  setup() {
+    return { v$: useVuelidate() }
+  },
+
+  data() {
+    return {
+      error: null,
+      pageDetails: {},
+    }
+  },
+
+  async created() {
+    this.getPageDetails(this.$route)
+  },
+
+  watch: {
+    $route(to, from) {
+      this.getPageDetails(to, from)
+    },
+  },
+
+  methods: {
+    async submitWithValidation(submit) {
+      if (!(await this.v$.$validate())) {
+        return
+      }
+      const [nextRoute, error] = await submit(this.getFormData())
+      if (error) {
+        this.error = error
+        return
+      }
+      this.$router.push({ path: nextRoute })
+    },
+    getFormData() {
+      const form = document.getElementById('form')
+      return new FormData(form)
+    },
+    getPageDetails(to, from) {
+      this.pageDetails = SignUpService.getPageDetails(to, from)
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.form {
+  max-width: 650px;
+}
+
+h1 {
+  font-size: 24px;
+  font-weight: 500;
+}
+
+p {
+  margin-bottom: 12px;
+}
+
+.button-narrow {
+  max-width: 185px;
+}
+
+.screen-narrow {
+  max-width: 512px;
+}
+
+.updog-crying {
+  height: 185px;
+}
+</style>
