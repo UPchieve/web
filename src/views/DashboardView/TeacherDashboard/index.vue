4fe5405acd0591e6a1345feacdc2f8d95b356748
Alex L <alex.lindsay@upchieve.org>
2024-06-04 15:53:21 +0000

    Add teacher dashboard boilerplate.

---
+++ src/views/DashboardView/TeacherDashboard/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="teacher-dashboard">
+    <dashboard-banner />
+    <div class="main">
+      <p>Nothing here yet!</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import DashboardBanner from '../DashboardBanner.vue'
+
+export default {
+  name: 'teacher-dashboard',
+  components: {
+    DashboardBanner,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.teacher-dashboard {
+  padding: 40px 15px;
+}
+
+.main {
+  @include flex-container(column, center, center);
+
+  margin-top: 40px;
+}
+</style>
