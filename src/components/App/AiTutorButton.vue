5e956fcf622afa3d47f7ccf7c783a8b7419da04b
Chris <chris.shaw@upchieve.org>
2024-10-15 14:10:23 +0000

    Clean up styles on ai button

--- src/components/App/AiTutorButton.vue
+++ src/components/App/AiTutorButton.vue
@@ -8,21 +8,21 @@ import { EVENTS } from '@/consts'
 onMounted(() => {
   AnalyticsService.captureEvent(EVENTS.AI_TUTOR_STAND_ALONE_BUTTON_SEEN)
 })
+
+const sendClickToAnalytics = () => {
+  AnalyticsService.captureEvent(EVENTS.AI_TUTOR_STAND_ALONE_BUTTON_CLICKED)
+}
 </script>
 
 <template>
   <large-button
     class="ai-bot-button"
-    @click="
-      () => {
-        AnalyticsService.captureEvent(
-          EVENTS.AI_TUTOR_STAND_ALONE_BUTTON_CLICKED
-        )
-      }
-    "
+    @click="sendClickToAnalytics"
     routeTo="/ai-tutor-conversations"
   >
-    <chat-bot-icon class="icon" />Request AI help
+    <div class="content-wrapper">
+      <chat-bot-icon class="icon" />Request AI help
+    </div>
   </large-button>
 </template>
 
@@ -30,12 +30,24 @@ onMounted(() => {
 .ai-bot-button {
   background-color: $c-information-blue;
   color: white;
-  padding: 4px 8px;
+  padding: 4px 0.8em 4px 5px;
   border-radius: 2em;
   margin-top: -44px !important;
   margin-bottom: 18px;
   @include breakpoint-below('medium') {
     margin-top: 0 !important;
   }
+  justify-content: initial;
+}
+
+/*fully own alignment of icon and text*/
+.content-wrapper {
+  display: flex;
+  align-items: center;
+}
+
+.icon {
+  /* Doesn't match styles but looks good */
+  margin-inline-end: 0.6em;
 }
 </style>
