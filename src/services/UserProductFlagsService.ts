9c73b8678b1ecf43eefcb2e0a9ce3f67141552cd
Trey Stevens <treykstevens.tks@gmail.com>
2025-01-09 21:19:24 +0000

    Impact study survey modal

--- src/services/UserProductFlagsService.ts
+++ src/services/UserProductFlagsService.ts
@@ -2,6 +2,7 @@ import NetworkService from './NetworkService'
 import AnalyticsService from './AnalyticsService'
 import { EVENTS } from '../consts'
 import type { Store } from 'vuex'
+import type { AxiosError } from 'axios'
 
 // TODO: Fix `store` type
 export async function enrollStudentToIncentiveProgram(
@@ -21,4 +22,17 @@ export async function enrollStudentToIncentiveProgram(
   store.dispatch('productFlags/addToProductFlags', {
     fallIncentiveEnrollmentAt,
   })
+}
+
+// TODO: Add analytic events and get an enrollment date for the study and push to PostHog
+// TODO: We should be handling the error status within the NetworkService instead of here
+export async function impactStudyEnrollment(surveyId: number) {
+  try {
+    return await NetworkService.impactStudyEnrollment(surveyId)
+  } catch (err) {
+    const error =
+      ((err as AxiosError).response?.data as { err?: string })?.err ??
+      'Unknown error'
+    throw new Error(error)
+  }
 }
