2b894e5f20672c68199e4b7d033f051ca995b2df
Alex Lindsay <alex.lindsay@upchieve.org>
2025-03-17 21:16:11 +0000

    Update deployment to dev/local with better version.

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -eux
+
+DATETIME=$(date '+%Y-%m-%d:%H-%M-%S')
+EMAIL=$(op account list --format json | jq -r '.[0].email')
+USER=${EMAIL%@*}
+VERSION="${DATETIME}_${USER}"
+
+echo "{\"version\": \"${VERSION}\"}" > public/version.json
+
+if [ "$1" = 'dev' ]; then
+  doppler run --token $(op read op://engineering/doppler-high-line-dev/password) -- npm run build && \
+  swa deploy \
+    --deployment-token $(op read op://engineering/swa-token-high-line-dev/password) \
+    --resource-group development-resource-group \
+    --app-name development-static-web-app \
+    --env production ./dist
+else
+    doppler run --token $(op read op://private/doppler-high-line/password) -- npm run build && \
+    swa deploy \
+    --deployment-token $(op read op://private/swa-token-high-line/password) \
+    --resource-group $(op read op://private/doppler-high-line/resource-group) \
+    --app-name $(op read op://private/doppler-high-line/app-name) \
+    --env production ./dist
+fi
