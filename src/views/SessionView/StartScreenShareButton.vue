04db22a74731157ef6e08b987c43a6a8041dcbd6
Sammy Nave <sammy.nave@gmail.com>
2025-04-02 13:16:09 -0400

    Adds button seen event for screenshare

---
+++ src/views/SessionView/StartScreenShareButton.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import ScreenShareIcon from '@/assets/screen-share.svg'
+import { onMounted } from 'vue'
+import { EVENTS } from '@/consts'
+import AnalyticsService from '@/services/AnalyticsService'
+
+onMounted(() => {
+  AnalyticsService.captureEvent(
+    EVENTS.SCREENSHARE_USER_SAW_START_SCREENSHARE_BUTTON
+  )
+})
+</script>
+
+<template>
+  <ScreenShareIcon />
+</template>
