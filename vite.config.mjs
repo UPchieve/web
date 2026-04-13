3784c75b402e432f37734ac17bb33166affdd594
sammy nave <sammy.nave@gmail.com>
2026-04-13 17:05:10 +0000

    Dependency chunks

--- vite.config.mjs
+++ vite.config.mjs
@@ -97,4 +97,19 @@ export default defineConfig({
       },
     ],
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id
+              .toString()
+              .split('node_modules/')[1]
+              .split('/')[0]
+              .toString()
+          }
+        },
+      },
+    },
+  },
 })
