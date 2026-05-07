bb18f825fc4a94c0f7b301d618d0c1ccc3dc4fe2
Kasozi Wilson <kasozi.wilson@upchieve.org>
2026-05-07 13:43:55 -0400

    Stop gleap from popping up on contact us page

--- src/router.js
+++ src/router.js
@@ -90,7 +90,7 @@ import {
   shouldGoToGroup,
   shouldGoToPending,
 } from './views/NTHS/nths-route-helpers'
-import Gleap from 'gleap'
+//import Gleap from 'gleap'
 import NetworkService, { axiosInstance } from './services/NetworkService'
 import { UserType } from '@/services/SignUpService'
 import { beforeEnter as studentBeforeEnter } from '@/services/SignUpService/StudentSignUpService'
@@ -205,7 +205,7 @@ const routes = [
       },
     },
     beforeEnter: (to, from, next) => {
-      const instance = Gleap.getInstance()
+      /*  const instance = Gleap.getInstance()
       if (instance.initialized) {
         try {
           Gleap.open()
@@ -216,7 +216,9 @@ const routes = [
         }
       } else {
         next()
-      }
+      }*/
+      //prevent gleap ddos
+      next()
     },
   },
   {
