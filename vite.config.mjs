ce2d61251812bc1c27b8164eb2bb34b32e5dc41b
Kasozi Wilson <kasozi.wilson@upchieve.org>
2026-06-05 12:46:20 -0400

    Revert "Update chunking strategy"

--- vite.config.mjs
+++ vite.config.mjs
@@ -153,17 +153,13 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks(id) {
-          if (!id.includes('node_modules')) return
-
-          const packagePath = id.split('node_modules/').pop()
-
-          // Handle scoped packages (@scope/package).
-          if (packagePath?.startsWith('@')) {
-            return packagePath.split('/').slice(0, 2).join('/')
+          if (id.includes('node_modules')) {
+            return id
+              .toString()
+              .split('node_modules/')[1]
+              .split('/')[0]
+              .toString()
           }
-
-          // Handle regular packages (package-name).
-          return packagePath.split('/')[0]
         },
       },
     },
